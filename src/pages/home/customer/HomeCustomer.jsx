026493f99ee8f21490548951217dba7afcc946bd
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-11-13 01:01:48 +0000

    WIP

--- src/pages/home/customer/HomeCustomer.jsx
+++ src/pages/home/customer/HomeCustomer.jsx
@@ -1,4 +1,4 @@
-import { ArrowLeftOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { ArrowLeftOutlined, MinusCircleOutlined, MinusOutlined, PlusCircleOutlined, PlusOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import { Button, Input } from "antd";
 import { useState } from "react";
 
@@ -6,7 +6,10 @@ function Product({ setIsOpen, image }) {
   return (
     <div className="overflow-hidden rounded-xl bg-white shadow-xl">
       <div className="relative h-32 bg-yellow-500">
-        <img src={image} className="absolute inset-0 h-full w-full object-cover"/>
+        <img
+          src={image}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
         <Button
           type="text"
           icon={<ShoppingCartOutlined className="text-white" />}
@@ -41,28 +44,53 @@ function Category({ image }) {
 }
 
 function ProductDetails({ setIsOpen }) {
+
+  const [quantity, setQuantity] = useState(10)
+
+  const increment = () => setQuantity(quantity => quantity + 1)
+  const decrement = () => setQuantity(quantity => Math.max(quantity-1, 10))
+
   return (
-    <div className="fixed inset-0 z-30 mx-auto  flex min-h-screen max-w-md flex-col bg-[#283618]">
+    <div className="fixed inset-0 z-30 mx-auto flex min-h-screen max-w-md flex-col bg-[#283618]">
       <div className="p-8 px-4">
         <ArrowLeftOutlined
           onClick={() => setIsOpen(false)}
           className="text-white"
         />
       </div>
-      <div className="flex flex-grow flex-col overflow-hidden rounded-t-3xl bg-white">
+      <div className="talipaapp-scrollbar flex flex-grow flex-col overflow-y-auto  rounded-t-3xl bg-white">
         <img
           src="https://via.placeholder.com/500x350"
           className="aspect-video w-full"
         />
 
-        <div className="p-4">
+        <div className="flex-grow p-4">
           <div className="text-2xl font-bold">Repolyo</div>
           <p className="mt-4 font-light">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
             error eligendi temporibus! Incidunt dignissimos eveniet aliquid sit
             voluptas quae amet esse laudantium nostrum deleniti, nemo corporis
-            ipsam aliquam quia eligendi?
+            ipsam aliquam quia eligendi? Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Repellat asperiores, quia est corrupti doloribus,
+            ut cupiditate autem accusamus in quis a perspiciatis eos at quaerat
+            dolores? Quia ullam aliquid quidem. Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Repellat asperiores, quia est corrupti
+            doloribus, ut cupiditate autem accusamus in quis a perspiciatis eos
+            at quaerat dolores? Quia ullam aliquid quidem.
           </p>
+        </div>
+        <div className="sticky bottom-0 bg-white p-4">
+          <div className="flex justify-between">
+            <span className="text-lg font-bold">₱{quantity*10}.00</span>
+            <div className="flex items-center gap-1">
+              <span onClick={decrement}><MinusCircleOutlined /></span>
+              <span>{quantity}kg</span>
+              <span onClick={increment}><PlusCircleOutlined /></span>
+            </div>
+          </div>
+          <button className="mt-2 w-full rounded bg-[#606C38] px-8 py-2 text-sm font-semibold text-white">
+            Purchase
+          </button>
         </div>
       </div>
     </div>
@@ -70,7 +98,7 @@ function ProductDetails({ setIsOpen }) {
 }
 
 export default function HomeCustomer() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="p-4">
@@ -106,15 +134,42 @@ export default function HomeCustomer() {
         <div className="text-lg font-semibold">Whats on the market?</div>
 
         <div className="mt-4 grid grid-cols-2 gap-2">
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400"/>
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
-          <Product setIsOpen={setIsOpen} image="https://via.placeholder.com/400x400" />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
+          <Product
+            setIsOpen={setIsOpen}
+            image="https://via.placeholder.com/400x400"
+          />
         </div>
       </div>
 
