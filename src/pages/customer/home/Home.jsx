192bddd85cf563a5754b4e90fa057f90945cb01a
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-11-16 16:12:44 +0000

    Fix Category Tab in Homepage

--- src/pages/customer/home/Home.jsx
+++ src/pages/customer/home/Home.jsx
@@ -4,40 +4,97 @@ import {
   SearchOutlined,
   ShoppingCartOutlined,
 } from "@ant-design/icons";
-import { Badge } from "antd";
+import { Badge, Empty, Spin } from "antd";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { sleep } from "../../../helpers/utils";
+import categories from "./homeData";
 
-function SubcategoryCard() {
+function SubcategoryCard({ name, image }) {
   return (
     <div className="bg-white p-4 text-center shadow">
       <Link
         to="/categories/1"
         className="mb-2 block font-semibold text-current"
       >
-        Apples
+        {name}
       </Link>
       <div className="aspect-square w-full rounded bg-slate-400"></div>
     </div>
   );
 }
 
+function CategoryTab({ selectedCategory, setSelectedCategory }) {
+  return (
+    <div className="talipaapp-scrollbar sticky top-0 flex overflow-x-auto bg-white px-4 pt-2 shadow">
+      {categories.map(({ name }, i) => (
+        <button
+          onClick={() => setSelectedCategory(i)}
+          key={name}
+          className={`whitespace-nowrap border-b-2 px-4 py-2 ${
+            selectedCategory === i
+              ? "border-primary text-primary"
+              : "border-white"
+          }`}
+        >
+          {name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function SubCategories({ category, isLoading }) {
+  const { subcategory } = category;
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center my-8">
+        <Spin size="large" tip="Fetching data"/>
+      </div>
+    )
+  }
+
+  if (subcategory.length === 0) {
+    return <Empty />;
+  }
+
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      {subcategory.map(({ name, image }) => (
+        <SubcategoryCard key={name} name={name} image={image} />
+      ))}
+    </div>
+  );
+}
+
 export default function HomeCustomer() {
+  const [selectedCategory, setSelectedCategory] = useState(0);
+  const [isLoading, setIsLoading] = useState(false)
+
+  const changeCategory = async(categoryIndex) => {
+    setIsLoading(true)
+    await sleep(500)
+    setIsLoading(false)
+    setSelectedCategory(categoryIndex)
+  }
+
   return (
     <>
       <div className="bg-white px-4">
         <div className="flex items-center justify-between py-4">
           <h1 className="text-xl font-bold">Home</h1>
           <div className="space-x-4">
-            <Link to='/notifications'>
+            <Link to="/notifications">
               <BellOutlined style={{ fontSize: 24 }} />
             </Link>
             <Badge count={3}>
-              <Link to='/likes'>
+              <Link to="/likes">
                 <HeartOutlined style={{ fontSize: 24 }} />
               </Link>
             </Badge>
             <Badge count={2}>
-              <Link to='/cart'>
+              <Link to="/cart">
                 <ShoppingCartOutlined style={{ fontSize: 24 }} />
               </Link>
             </Badge>
@@ -76,34 +133,13 @@ export default function HomeCustomer() {
         </div>
       </div>
 
-      <div className="pt-2 flex overflow-x-auto bg-white px-4 sticky top-0 shadow">
-        <button className="whitespace-nowrap border-b-2 border-primary px-4 py-2 text-primary">
-          Fruits
-        </button>
-        <button className="whitespace-nowrap border-b-2 border-white px-4 py-2">
-          Vegetables
-        </button>
-        <button className="whitespace-nowrap border-b-2 border-white px-4 py-2">
-          Meats
-        </button>
-        <button className="whitespace-nowrap border-b-2 border-white px-4 py-2">
-          Root Crops
-        </button>
-      </div>
+      <CategoryTab
+        selectedCategory={selectedCategory}
+        setSelectedCategory={changeCategory}
+      />
 
-      <div className="py-4">
-        <div className="grid grid-cols-2 gap-2">
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-          <SubcategoryCard />
-        </div>
+      <div className="py-4">        
+        <SubCategories isLoading={isLoading} category={categories[selectedCategory]} />
       </div>
     </>
   );
