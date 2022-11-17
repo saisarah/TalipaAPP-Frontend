f8e0de4ed9b50d0c2392a25088d8513b23118557
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-11-17 03:28:03 +0000

    WIP

--- src/pages/customer/cart/Cart.jsx
+++ src/pages/customer/cart/Cart.jsx
@@ -1,100 +1,33 @@
 import {
   ArrowLeftOutlined,
-  MinusOutlined,
   RightOutlined,
   ShopOutlined,
 } from "@ant-design/icons";
-import { Button, Checkbox } from "antd";
+import { Button, Checkbox, Empty } from "antd";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import MapPinOutline from "../../../icons/heroicons/MapPinOutline";
+import CartGroup from "./CartGroup";
+import CartItem from "./CartItem";
 
-export default function Cart() {
+function All() {
   return (
-    <div className="mx-auto min-h-screen max-w-md pb-14">
-      <div className="bg-white px-4">
-        <div className="flex items-center gap-4 py-4">
-          <Link to="/" className="text-current">
-            <ArrowLeftOutlined style={{ fontSize: 20 }} />
-          </Link>
-          <span className="text-lg font-semibold">My Cart</span>
-        </div>
-      </div>
-      <div className="flex bg-white">
-        <button className="flex-grow border-b border-primary py-2 px-4">
-          All
-        </button>
-        <button className="flex-grow border-b border-white py-2 px-4">
-          Buy Again
-        </button>
-      </div>
+    <>
+      <div className="my-4 flex flex-col gap-4">
+        <CartGroup name="Sarah Grace Arlyn">
+          <CartItem name="Kalabasa" />
+          <CartItem name="Apple" />
+          <CartItem name="Mangga" />
+        </CartGroup>
 
-      <div className="mt-4 bg-white p-4">
-        <div className="flex items-center border-b border-gray-200 pb-2">
-          <Checkbox className="font-semibold" />
+        <CartGroup name="Lenard Mangay-ayam">
+          <CartItem name="Repolyo" />
+          <CartItem name="Broccoli" />
+        </CartGroup>
 
-          <div className="flex items-center">
-            <ShopOutlined className="mx-2" />
-            <span className="font-bold">Sarah Grace Arlyn</span>
-            <RightOutlined className="mx-2" style={{ fontSize: 12 }} />
-          </div>
-        </div>
-        <div className="pt-2">
-          {/*  */}
-          <div className="flex items-center gap-2">
-            <Checkbox />
-            <div className="flex flex-grow gap-2 py-2">
-              <img
-                className="aspace-square w-1/4 self-start rounded"
-                src="https://via.placeholder.com/150"
-              />
-              <div className="flex flex-grow flex-col">
-                <span className="text-base">Kalabasa</span>
-                <div className="flex items-center gap-2 overflow-hidden overflow-ellipsis text-xs text-slate-400">
-                  <MapPinOutline />
-                  <span className=" whitespace-nowrap ">
-                    Sta. Rosa City, Laguna hkkn
-                  </span>
-                </div>
-                <span className="text-lg font-bold text-primary">₱70.00</span>
-                <div className="mt-2 flex">
-                  <button className="bg-slate-100 px-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-2 w-2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M19.5 12h-15"
-                      />
-                    </svg>
-                  </button>
-                  <span className="px-2 text-sm">1</span>
-                  <button className="bg-slate-100 px-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-2 w-2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 4.5v15m7.5-7.5h-15"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CartGroup name="Paolo Ortega">
+          <CartItem name="Grapes" />
+          <CartItem name="Avocado" />
+        </CartGroup>
       </div>
 
       <div className="items-cemter fixed bottom-0 flex w-full max-w-md bg-white shadow">
@@ -111,6 +44,45 @@ export default function Cart() {
           </Button>
         </div>
       </div>
+    </>
+  );
+}
+
+export default function Cart() {
+  const [tab, setTab] = useState("all");
+
+  return (
+    <div className="mx-auto min-h-screen max-w-md pb-14">
+      <div className="sticky top-0 bg-white z-10">
+        <div className="bg-white px-4">
+          <div className="flex items-center gap-4 py-4">
+            <Link to="/" className="text-current">
+              <ArrowLeftOutlined style={{ fontSize: 20 }} />
+            </Link>
+            <span className="text-lg font-semibold">My Cart</span>
+          </div>
+        </div>
+        <div className="flex">
+          <button
+            onClick={() => setTab("all")}
+            className={`flex-grow border-b py-2 px-4 ${
+              tab === "all" ? "border-primary" : ""
+            }`}
+          >
+            All
+          </button>
+          <button
+            onClick={() => setTab("buy_again")}
+            className={`flex-grow border-b py-2 px-4 ${
+              tab === "buy_again" ? "border-primary" : ""
+            }`}
+          >
+            Buy Again
+          </button>
+        </div>
+      </div>
+
+      {tab === "all" ? <All /> : <Empty className="my-16" />}
     </div>
   );
 }
