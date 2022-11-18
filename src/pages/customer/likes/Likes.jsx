9666ccb6bdfbb34cc9f5625f3b7d27de1c776ff1
Uno-count <paonemorfi@gmail.com>
2022-11-18 20:48:17 +0800

    update likes page

--- src/pages/customer/likes/Likes.jsx
+++ src/pages/customer/likes/Likes.jsx
@@ -1,10 +1,24 @@
+import { FileSearchOutlined, SearchOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 import UnderConstruction from "../../../components/UnderConstruction";
 
-export default function Likes()
-{
+export default function Likes() {
   return (
-    <div className="max-w-md mx-auto min-h-screen">
-      <UnderConstruction showHomeBtn/>
+    <div className="bg-white font-extrabold">
+      <SearchOutlined className="text-red-900" style={{ fontSize: 100 }} />
+      <FileSearchOutlined />
+      <p>like page</p>
+
+      <Button type="primary">SUBMIT</Button>
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+</svg>
+
+
+
+
     </div>
-  )
+  );
 }
