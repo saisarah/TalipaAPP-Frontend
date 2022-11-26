0c9f4e6f08064fc4237afcdc80cc2b6ada9be705
Ruel Almonia <qweqwzxc01@gmail.com>
2022-11-26 20:17:25 +0800

    landing-page

--- src/pages/customer/home/Home.jsx
+++ src/pages/customer/home/Home.jsx
@@ -5,87 +5,59 @@ import {
   ShoppingCartOutlined,
 } from "@ant-design/icons";
 import { Badge } from "antd";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { sleep } from "../../../helpers/utils";
 import CategoryTab from "./CategoryTab";
 import categories from "./homeData";
 import SubCategories from "./SubCategories";
 
+function Card(props) {
+  return (
+    <div className="flex flex-col items-center gap-2 rounded-md bg-white py-4 shadow-md">
+      <img src={props.src} className="h-20" />
+      <span className="text-lg font-bold">{props.title}</span>
+    </div>
+  );
+}
+
+function Banner(props) {
+  return (
+      <img src={props.src} className="h-40 w-auto rounded-md shadow-md"/>
+  )
+}
 
 export default function HomeCustomer() {
   const [selectedCategory, setSelectedCategory] = useState(0);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-  const changeCategory = async(categoryIndex) => {
-    setIsLoading(true)
-    await sleep(500)
-    setIsLoading(false)
-    setSelectedCategory(categoryIndex)
-  }
+  const changeCategory = async (categoryIndex) => {
+    setIsLoading(true);
+    await sleep(500);
+    setIsLoading(false);
+    setSelectedCategory(categoryIndex);
+  };
 
   return (
     <>
-      <div className="bg-white px-4">
-        <div className="flex items-center justify-between py-4">
-          <h1 className="text-xl font-bold">Home</h1>
-          <div className="space-x-4">
-            <Link to="/notifications">
-              <BellOutlined style={{ fontSize: 24 }} />
-            </Link>
-            <Badge count={3}>
-              <Link to="/likes">
-                <HeartOutlined style={{ fontSize: 24 }} />
-              </Link>
-            </Badge>
-            <Badge count={2}>
-              <Link to="/cart">
-                <ShoppingCartOutlined style={{ fontSize: 24 }} />
-              </Link>
-            </Badge>
-          </div>
-        </div>
-
-        <div className="talipaapp-scrollbar overflow-auto py-2">
-          <div className="flex gap-4">
-            <img
-              className="aspect-video h-36 rounded shadow"
-              src="https://via.placeholder.com/500x350"
-            />
-            <img
-              className="aspect-video h-36 rounded shadow"
-              src="https://via.placeholder.com/500x350"
-            />
-            <img
-              className="aspect-video h-36 rounded shadow"
-              src="https://via.placeholder.com/500x350"
-            />
-          </div>
-        </div>
-
-        <div className="py-2">
-          <div className="flex gap-2 rounded bg-slate-200 p-3">
-            <SearchOutlined
-              style={{ fontSize: 20 }}
-              className="text-slate-500"
-            />
-            <input
-              className="flex-grow bg-transparent focus:outline-none"
-              type="search"
-              placeholder="Search for fruits, vegetables, etc..."
-            />
-          </div>
-        </div>
+      <div className="flex flex-row items-center gap-2 px-4 py-2">
+        <img src="/assets/logo.png" className="h-10" />
+        <span className="whitespace-nowrap text-lg font-bold text-primary">
+          Hello Juan
+        </span>
       </div>
 
-      <CategoryTab
-        selectedCategory={selectedCategory}
-        setSelectedCategory={changeCategory}
-        categories={categories}
-      />
+      <div className="flex flex-row gap-4 overflow-x-auto talipaapp-scrollbar mx-4">
+        <Banner src="/assets/images/banner1.svg"/>
+        <Banner src="/assets/images/banner2.svg"/>
+      </div>
 
-      <div className="py-4">        
-        <SubCategories isLoading={isLoading} category={categories[selectedCategory]} />
+      <div className="mt-4 grid grid-cols-2 gap-2 mx-4">
+        <Card title="Seller" src="/assets/images/seller.svg"/>
+        <Card title="Buyer" src="/assets/images/buyer.svg"/>
+        <Card title="Association" src="/assets/images/association.svg"/>
+        <Card title="Public Market" src="/assets/images/market.svg"/>
+        <Card title="News" src="/assets/images/news.svg"/>
       </div>
     </>
   );
