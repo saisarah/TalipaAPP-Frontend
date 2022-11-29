e901b84cfeafabac5f94e91d3a3111774fe94e2d
Josh <villanuevajoshua27@gmail.com>
2022-11-29 18:18:11 +0800

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,28 @@
 import { Button, Checkbox, Form, Input, Spin } from "antd";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-
   return (
-    <div className="max-w-md mx-auto min-h-screen">
-      Login Page
+    <div className="mx-auto min-h-screen max-w-md text-center">
+      <img src="/assets/images/logo(1).svg" alt="" />
+
+      <p className="">Enter your phone number to get start.</p>
+      <Input
+        type="number"
+        className="btn-outlined"
+        placeholder="+63 9** *** ****"
+      />
+      <Link to={"/verification"}>
+        <Input
+          type="submit"
+          className="mb-6 bg-primary text-white"
+          value="Next"
+        />
+      </Link>
+
+      <p>Dont have an account</p>
+      <Link to={"/register"}>Sign up here</Link>
     </div>
   );
 }
