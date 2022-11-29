d8f464af6647e59863a5c87dbf975de38d94c2e4
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-11-29 16:48:18 +0000

    Refactor Routes

---
+++ src/routes/routes.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Login from "../pages/login/Login";
+import vendorRoutes from "./vendorRoutes";
+
+export default [
+  {
+    element: <Outlet />,
+    children: vendorRoutes,
+  },
+  {
+    path: "/farmer",
+    element: <Outlet />,
+    children: farmerRoutes,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <div>Register</div>,
+  },
+];
