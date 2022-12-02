cfee691d69c5c05288f3ff739282fe1957a3fe0f
Josh <villanuevajoshua27@gmail.com>
2022-12-02 21:11:21 +0800

    keyboard

--- src/pages/login/Verification.jsx
+++ src/pages/login/Verification.jsx
@@ -1,5 +1,10 @@
 import { Button, Checkbox, Form, Input, Spin } from "antd";
-import { LockOutlined, UserOutlined, LeftOutlined } from "@ant-design/icons";
+import {
+  LockOutlined,
+  UserOutlined,
+  LeftOutlined,
+  BorderBottomOutlined,
+} from "@ant-design/icons";
 import OTPInput, { ResendOTP } from "otp-input-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -9,7 +14,7 @@ export default function Login() {
 
   return (
     <>
-      <div className="mx-auto min-h-screen max-w-md p-5">
+      <div className="mx-auto min-h-screen max-w-md bg-white p-5">
         <Link to={"/login"}>
           <LeftOutlined />
         </Link>
@@ -17,10 +22,24 @@ export default function Login() {
 
         <div className="my-6">
           <h1 className="text-xl font-bold">We sent you an SMS code</h1>
-          <p>on number: +63 9** **** ****</p>
+          <p>
+            on number: <span className="text-primary">+63 9** **** ****</span>
+          </p>
         </div>
 
+        <br />
+        <br />
+        <br />
+        <br />
         <OTPInput
+          inputStyles={{
+            // border: "1px solid black",
+            borderRadius: "3px",
+            width: "60px",
+            height: "60px",
+            boxShadow: "2px 2px 2px 2px #ccc",
+            outlineColor: "green",
+          }}
           className="my-auto items-center justify-center"
           value={OTP}
           onChange={setOTP}
@@ -28,10 +47,42 @@ export default function Login() {
           OTPLength={4}
           otpType="number"
           disabled={false}
-          secure
+          // secure
         />
         <br />
-        <ResendOTP onResendClick={() => console.log("Resend clicked")} />
+        {/* <ResendOTP onResendClick={() => console.log("Code not received?")} /> */}
+
+        <div className="mb-6 text-center">
+          <Link>Code not received?</Link>
+        </div>
+
+        <div>
+          <div className="mb-10 grid grid-cols-3 items-center gap-4">
+            <Button style={{ border: "none" }}>1</Button>
+            <Button style={{ border: "none" }}>2</Button>
+            <Button style={{ border: "none" }}>3</Button>
+          </div>
+          <div className="mb-10 grid grid-cols-3 items-center gap-4">
+            <Button style={{ border: "none" }}>4</Button>
+            <Button style={{ border: "none" }}>5</Button>
+            <Button style={{ border: "none" }}>6</Button>
+          </div>
+          <div className="mb-10 grid grid-cols-3 items-center gap-4">
+            <Button style={{ border: "none" }}>7</Button>
+            <Button style={{ border: "none" }}>8</Button>
+            <Button style={{ border: "none" }}>9</Button>
+          </div>
+          <div className="mb-10 grid grid-cols-3 items-center gap-4">
+            <Button style={{ border: "none" }}></Button>
+            <Button style={{ border: "none" }}>0</Button>
+            <Button
+              className="grid justify-items-center"
+              style={{ border: "none" }}
+            >
+              <img src="/assets/images/icon _delete.svg" alt="" />
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Login from "../pages/login/Login";
 import vendorRoutes from "./vendorRoutes";
 import farmerRoutes from "./farmerRoutes";
-
+import Verification from "../pages/login/Verification";
 export default [
   {
     element: <Outlet />,
@@ -16,6 +16,10 @@ export default [
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/verification",
+    element: <Verification />,
   },
   {
     path: "/register",
