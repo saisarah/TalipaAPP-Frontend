2bac92e10a81c16c9f4778035d24aa6374624126
Josh <villanuevajoshua27@gmail.com>
2022-12-02 18:05:55 +0800

    fixed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,25 +4,35 @@ import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="mx-auto min-h-screen max-w-md text-center">
+    <div className="mx-auto min-h-screen max-w-md bg-white p-4 text-center">
       <img src="/assets/images/logo(1).svg" alt="" />
 
       <p className="">Enter your phone number to get start.</p>
       <Input
-        type="number"
-        className="btn-outlined"
-        placeholder="+63 9** *** ****"
+        type="text"
+        placeholder="+63 9"
+        className="mb-12"
+        style={{ border: "none", borderBottom: "1px solid #000" }}
       />
       <Link to={"/verification"}>
         <Input
           type="submit"
-          className="mb-6 bg-primary text-white"
+          className="my-12 mb-6 bg-primary text-white"
           value="Next"
         />
       </Link>
 
       <p>Dont have an account</p>
       <Link to={"/register"}>Sign up here</Link>
+      <br />
+      <Link
+        to={"/"}
+        className="fixed
+             inset-x-0 bottom-0 p-4
+             "
+      >
+        Help Center
+      </Link>
     </div>
   );
 }
