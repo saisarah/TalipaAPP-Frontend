7259bc5444d86ac3b17886d3fb851e7e4f966f45
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-12-02 15:12:37 +0000

    Fix some design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,33 +4,34 @@ import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="mx-auto min-h-screen max-w-md bg-white p-4 text-center">
-      <img src="/assets/images/logo(1).svg" alt="" />
+    <div className="mx-auto flex min-h-screen max-w-md flex-col bg-slate-50 p-4 px-8 text-center">
+      <img src="/assets/images/logo.svg" alt="" />
 
       <p className="">Enter your phone number to get start.</p>
-      <Input
-        type="text"
-        placeholder="+63 9"
-        className="mb-12"
-        style={{ border: "none", borderBottom: "1px solid #000" }}
-      />
-      <Link to={"/verification"}>
+      <div className="flex-grow">
         <Input
-          type="submit"
-          className="my-12 mb-6 bg-primary text-white"
-          value="Next"
+          size="large"
+          className="rounded"
+          addonBefore="+63"
+          placeholder="923 4567 890"
         />
-      </Link>
 
-      <p>Dont have an account</p>
-      <Link to={"/register"}>Sign up here</Link>
-      <br />
-      <Link
-        to={"/"}
-        className="fixed
-             inset-x-0 bottom-0 p-4
-             "
-      >
+        <Link to={"/verification"}>
+          <Input
+            type="submit"
+            className="my-12 mb-6 bg-primary text-white"
+            value="Next"
+            size="large"
+          />
+        </Link>
+
+        <p>
+          <span>Dont have an account</span><br/>
+          <span>Sign up <Link className="text-primary" to={"/register"}>here</Link></span>
+        </p>
+      </div>
+
+      <Link to={"/"} className="">
         Help Center
       </Link>
     </div>
