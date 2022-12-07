1a0f4e77d817cb91078fdd29d4da5808301be590
Uno-count <paonemorfi@gmail.com>
2022-12-07 21:04:23 +0800

    registration farmer

--- src/pages/register/RegistrationForm.jsx
+++ src/pages/register/RegistrationForm.jsx
@@ -1,20 +1,15 @@
 import { LeftOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Select, Steps } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Button, Form, Input, PageHeader, Select, Steps } from "antd";
+import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
 import {
-  regions,
+  regions as fetchRegions,
   provinces,
   cities,
   barangays,
 } from "select-philippines-address";
-
-regions().then((region) => console.log(region));
-regionByCode("01").then((region) => console.log(region.region_name));
-provinces("01").then((province) => console.log(province));
-provincesByCode("01").then((province) => console.log(province));
-provinceByName("Rizal").then((province) => console.log(province.province_code));
-cities("0128").then((city) => console.log(city));
-barangays("052011").then((barangays) => console.log(barangays));
 
 function PersonalInformationForm({ setStep }) {
   return (
@@ -83,14 +78,42 @@ function PersonalInformationForm({ setStep }) {
 }
 
 function AddressForm({ setStep }) {
+  const { data: regions, isLoading: regionsIsLoading } = useQuery(
+    ["region"],
+    fetchRegions,
+    {
+      select: (data) =>
+        data.map((row) => ({ label: row.region_name, value: row.region_code })),
+    }
+  );
+
+  const [selectedRegion, setSelectedRegion] = useState();
+
+  // const [regions, setRegions] = useState([]);
+
+  // useEffect(
+  //   function () {
+  //     async function fetchRegions() {
+  //       const regions = await axios.get('https://isaacdarcilla.github.io/philippine-addresses/region.json')
+  //       setRegions(regions.data.map((region) => ({ value: region.region_code, label: region.region_name })))
+  //     }
+  //     fetchRegions()
+  //   },
+  //   []
+  // );
+
   return (
     <Form layout="vertical" onFinish={() => setStep(2)}>
       <Form.Item className="mb-4" label="Region" hasFeedback initialValue="">
-        <Select size="large" className="rounded">
-          <Select.Option value=""></Select.Option>
-          <Select.Option value="male">Region#</Select.Option>
-          <Select.Option value="female">Region#</Select.Option>
-        </Select>
+        <Select
+          value={selectedRegion}
+          onChange={(region) => setSelectedRegion(region)}
+          size="large"
+          className="rounded"
+          placeholder="Select Region"
+          options={regions}
+          loading={regionsIsLoading}
+        />
       </Form.Item>
 
       <Form.Item className="mb-4" label="Province" hasFeedback initialValue="">
@@ -161,20 +184,23 @@ export default function RegistrationForm({ reset }) {
   const [step, setStep] = useState(0);
 
   return (
-    <div className="p-4">
-      <div className="mb-6 flex items-center">
-        <Button type="text" onClick={reset} icon={<LeftOutlined />} />
-        <div className="text-2xl font-bold">Create Your Account</div>
+    <>
+      <PageHeader title="home" className="shadow" />
+      <div className="p-4">
+        <div className="mb-6 flex items-center">
+          <Button type="text" onClick={reset} icon={<LeftOutlined />} />
+          <div className="text-2xl font-bold">Create Your Account</div>
+        </div>
+        <Steps
+          items={[{ title: "Personal Information" }, { title: "Address" }]}
+          size="small"
+          className="mb-8"
+          current={step}
+          direction="vertical"
+        />
+        {step === 0 && <PersonalInformationForm setStep={setStep} />}
+        {step === 1 && <AddressForm />}
       </div>
-      <Steps
-        items={[{ title: "Personal Information" }, { title: "Address" }]}
-        size="small"
-        className="mb-8"
-        current={step}
-        direction="vertical"
-      />
-      {step === 0 && <PersonalInformationForm setStep={setStep} />}
-      {step === 1 && <AddressForm />}
-    </div>
+    </>
   );
 }
