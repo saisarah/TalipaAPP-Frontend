e5ce9c0a7bc2460bf9d87d7631af88cd7e716a67
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2022-12-12 22:29:19 +0800

    create-info

--- src/pages/farmer/Feed/CreateInfo.jsx
+++ src/pages/farmer/Feed/CreateInfo.jsx
@@ -1,3 +1,24 @@
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import PageHeader from "../../../components/PageHeader";
+
 export default function CreateInfo() {
-  return "lorem ipsum dolor sit amet, consectetur adip";
+  return (
+    <div>
+      <div className="mt-40 mb-14 flex justify-center">
+        <img src="/assets/images/blog-post.png" />
+      </div>
+      <div className="text-center">
+        <h1 className="text-xl font-bold">Create Post</h1>
+        <p className="">
+          Farmer groups are group of individual farmers, an association,
+          cooperative or any legal entity with a common farming interest.
+        </p>
+        <Button className="mt-2 text-lg text-[#739559]" size={"large"}>
+          Create
+        </Button>
+      </div>
+    </div>
+  );
 }
