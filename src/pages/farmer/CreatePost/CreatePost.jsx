25488035803bb8235a5da95da26a5ec8e2b3bdd0
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2022-12-13 01:42:42 +0800

    upload

--- src/pages/farmer/CreatePost/CreatePost.jsx
+++ src/pages/farmer/CreatePost/CreatePost.jsx
@@ -1,10 +1,11 @@
-import { ArrowLeftOutlined } from "@ant-design/icons";
-import { Form, Input, Select, Space } from "antd";
+import { ArrowLeftOutlined, UploadOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Select, Space, Upload } from "antd";
 import { Link } from "react-router-dom";
 import PageHeader from "../../../components/PageHeader";
 import { crops, PricingType } from "./post-data";
 import { rules } from "./rules";
 import { unit } from "./post-data";
+import TextArea from "antd/lib/input/TextArea";
 
 function SelectComponent({ label, name, placeholder, options }) {
   return (
@@ -16,9 +17,38 @@ function SelectComponent({ label, name, placeholder, options }) {
 
 function InputComponent({ placeholder, label, name }) {
   return (
+    <Form.Item label={label} name={name} rules={rules.commodity}>
       <Input addonAfter="kg" placeholder={placeholder} size="large" />
+    </Form.Item>
   );
 }
+
+function TextAreaComponent({ placeholder, label, name }) {
+  return (
+    <Form.Item label={label} name={name}>
+      <TextArea rows={4} placeholder={placeholder} />
+    </Form.Item>
+  );
+}
+
+const props = {
+  name: "file",
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
 
 export default function CreatePost() {
   return (
@@ -39,28 +69,34 @@ export default function CreatePost() {
             placeholder="Select Commodity"
             options={crops}
           />
-        </Form>
-        <Form layout="vertical">
           <SelectComponent
             label="Unit"
             name="unit"
             placeholder="Select Unit"
             options={unit}
           />
-        </Form>
-        <Form layout="vertical">
+
           <SelectComponent
             label="Pricing Type"
             name="pricing type"
             placeholder="Select Pricing Type"
             options={PricingType}
           />
-        </Form>
+
           <InputComponent
             label="Total Stocks"
             name="total stocks"
             placeholder="Input Kilogram"
           />
+          <TextAreaComponent
+            label="Details"
+            name="details"
+            placeholder="Input Details"
+          />
+          <Upload {...props}>
+            <Button icon={<UploadOutlined />} size="large" className="w-full" block={true}>Click to Upload</Button>
+          </Upload>
+        </Form>
       </div>
     </div>
   );
