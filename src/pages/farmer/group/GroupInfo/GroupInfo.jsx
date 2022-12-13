604b2e9be665d9609405605ed26e0667695bc867
lenard123 <lenard.mangayayam@gmail.com>
2022-12-13 10:10:55 +0800

    fixes

--- src/pages/farmer/group/GroupInfo/GroupInfo.jsx
+++ src/pages/farmer/group/GroupInfo/GroupInfo.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { getGroup } from "../groups";
 import PageHeader from "../../../../components/PageHeader";
 import { ArrowLeftOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { Button, Spin } from "antd";
 import { useState } from "react";
 import Join from "../components/Join";
 import Forum from "../components/Forum";
@@ -22,50 +22,57 @@ export default function GroupInfo() {
     <div className="mx-auto min-h-screen max-w-md bg-white">
       <PageHeader
         left={
-          <Link to="/farmer">
+          <Link to="/farmer/groups">
             <ArrowLeftOutlined style={{ fontSize: "16px" }} />
           </Link>
         }
         title="Group"
       />
 
-      <div className="h-[80px] w-[100%] max-w-md flex-shrink-0 bg-slate-300"></div>
-      <img
-        className="mx-auto h-[75px] w-[75px] max-w-md flex-shrink-0 rounded-full bg-slate-300 ring"
-        style={{ marginTop: "-45px" }}
-      />
-      <div className="my-6 text-center">
-        <h1>Totong Lipay Farmers Association</h1>
-        <h1>Association</h1>
-        {/* <h1>{membersCount} Members</h1> */}
-        <p>200 Members</p>
-        <Button type="primary" className="mx-auto">
-          Join
-        </Button>
-      </div>
+      {isLoading ? (
+        <div className="flex items-center justify-center py-16">
+          <Spin tip="Fetching group informations" />
+        </div>
+      ) : (
+        <>
+          <div className="h-[80px] w-[100%] max-w-md flex-shrink-0 bg-slate-300"></div>
+          <img
+            className="mx-auto h-[75px] w-[75px] max-w-md flex-shrink-0 rounded-full bg-slate-300 ring"
+            style={{ marginTop: "-45px" }}
+          />
+          <div className="my-6 text-center">
+            <h1>{group.name}</h1>
+            <h1>{group.type}</h1>
+            <p>{group.members_count} Members</p>
+            <Button type="primary" className="mx-auto">
+              Join
+            </Button>
+          </div>
 
-      <div className="sticky top-0 grid h-16 grid-cols-2 bg-white text-lg shadow-md">
-        <button
-          onClick={() => setActive("join")}
-          className={`flex items-center justify-center ${
-            active === "join" ? "border-b border-primary text-primary" : ""
-          }`}
-        >
-          Info
-        </button>
+          <div className="sticky top-0 grid h-16 grid-cols-2 bg-white text-lg shadow-md">
+            <button
+              onClick={() => setActive("join")}
+              className={`flex items-center justify-center ${
+                active === "join" ? "border-b border-primary text-primary" : ""
+              }`}
+            >
+              Info
+            </button>
 
-        <button
-          onClick={() => setActive("forum")}
-          className={`flex items-center justify-center ${
-            active === "forum" ? "border-b border-primary text-primary" : ""
-          }`}
-        >
-          Forum
-        </button>
-      </div>
+            <button
+              onClick={() => setActive("forum")}
+              className={`flex items-center justify-center ${
+                active === "forum" ? "border-b border-primary text-primary" : ""
+              }`}
+            >
+              Forum
+            </button>
+          </div>
 
-      {active === "join" && <Join />}
-      {active === "forum" && <Forum />}
+          {active === "join" && <Join />}
+          {active === "forum" && <Forum />}
+        </>
+      )}
     </div>
   );
 }
