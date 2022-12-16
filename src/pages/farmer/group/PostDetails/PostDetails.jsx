89eaeff541c09a5ea8af04b2202788119b6a374a
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2022-12-16 23:52:15 +0800

    test

--- src/pages/farmer/group/PostDetails/PostDetails.jsx
+++ src/pages/farmer/group/PostDetails/PostDetails.jsx
@@ -1,7 +1,20 @@
+import { ArrowLeftOutlined } from "@ant-design/icons"
+import { Link } from "react-router-dom"
+import PageHeader from "../../../../components/PageHeader"
 
 
 export const PostDetails = () => {
     return (
-        <div className="mx-auto min-h-screen max-w-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio corrupti laboriosam earum enim ipsa. Animi ducimus modi libero labore quidem dolor facilis voluptatibus, cum quis deserunt nulla odit distinctio sed?</div>
+        <div className="mx-auto min-h-screen max-w-md bg-slate-50">
+      <PageHeader
+        left={
+          <Link to="/farmer/home">
+            <ArrowLeftOutlined style={{ fontSize: "16px" }} />
+          </Link>
+        }
+        title="Sarah Oben's Post"
+      />
+      <div className="flex flex-col items-center gap-2 bg-white py-4 mt-2 mx-2"></div>
+    </div>
     )
 }
