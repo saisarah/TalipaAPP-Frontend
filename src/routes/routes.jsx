78263fdecc54604692274800b44a490fe10f6012
lenard123 <lenard.mangayayam@gmail.com>
2023-01-21 09:16:13 +0000

    fixed: wrong imports

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,10 @@
 import vendorRoutes from "./vendorRoutes";
 import farmerRoutes from "./farmerRoutes";
-import VendorGate from "../components/VendorGate";
-import FarmerGate from "./gates/FarmerGate";
-import GuestGate from "../components/GuestGate";
 import guestRoutes from "./guestRoutes";
+import FarmerGate from "./gates/FarmerGate";
+// import GuestGate from "../components/GuestGate";
+import VendorGate from "./gates/VendorGate";
+import GuestGate from "./gates/GuestGate";
 
 export default [
   {
