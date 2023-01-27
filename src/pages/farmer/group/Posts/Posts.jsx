0bd08d27b797e3bc470434ae56de8c5d6371f968
lenard123 <lenard.mangayayam@gmail.com>
2023-01-27 02:21:49 +0000

    bug fixed: typo in importing

--- src/pages/farmer/group/Posts/Posts.jsx
+++ src/pages/farmer/group/Posts/Posts.jsx
@@ -7,7 +7,7 @@ import { About } from "../components/About";
 import { items } from "./post-data";
 import MemberPosts from "../components/MembersPosts";
 import PageHeader from "@/components/PageHeader";
-import groupIllustrationImg from "./images/group_illustration.png"
+import groupIllustrationImg from "../images/group_illustration.png"
 
 export const Posts = () => {
   const [active, setActive] = useState("forum");
