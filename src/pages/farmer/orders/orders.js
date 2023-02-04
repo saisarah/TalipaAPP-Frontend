e319c2b86d215083cec06c04f003514f2c9e05ff
Uno-count <paonemorfi@gmail.com>
2023-02-05 00:07:47 +0800

    useQuery orders

---
+++ src/pages/farmer/orders/orders.js
@@ -0,0 +1,49 @@
+import { sleep } from "@/helpers/utils"
+
+
+const orders = [
+    {
+        id: 1,
+        name: 'Midel Dela Cruz',
+        timestamp: '3 days ago',
+        order_name: 'Manga',
+        quantity: 100,
+        price: 50,
+        location: 'NCR, CALOOCAN CITY',
+        status: 'Pending',
+        total: 5000
+    },
+    {
+        id: 2,
+        name: 'Tony Gojar',
+        timestamp: '3 days ago',
+        order_name: 'Manga',
+        quantity: 100,
+        price: 50,
+        location: 'NCR, CALOOCAN CITY',
+        status: 'Pending',
+        total: 5000
+    }, 
+    {
+        id: 3,
+        name: 'Ben Bacalla',
+        timestamp: '3 days ago',
+        order_name: 'Manga',
+        quantity: 100,
+        price: 50,
+        location: 'NCR, CALOOCAN CITY',
+        status: 'Pending',
+        total: 5000
+    },
+    
+]
+
+export const getOrders = async () => {
+    await sleep(500)
+    return orders
+}
+
+export const getOrder = async (id) => {
+    const orders = await getOrders()
+    return orders.find(order => order.id == id)
+}
