9b44f330e6cc0ac82226b1b797f14bdaa256fedb
r-kyle <kylerubante@gmail.com>
2023-02-07 17:54:49 +0800

    Help Page Created

--- src/pages/help/Faq.jsx
+++ src/pages/help/Faq.jsx
@@ -21,43 +21,51 @@ return (
               
     <Collapse defaultActiveKey={['0']} onChange={onChange}>
       
-      <Panel header="What payment methods do you accept?" key="1">
+    <Panel header="Which courier will deliver my ordered items?" key="1">
+        <p>As of now, TalipaAPP only offers Transpotify as the only courier.</p>
+      </Panel>
+
+      <Panel header="Does TalipaAPP offers vouchers to its customers?" key="2">
+        <p>No, voucher system is not yet implemented.</p>
+      </Panel>
+
+      <Panel header="What payment methods do you accept?" key="3">
         <p>TalipaAPP only accepts cash on delivery and GCash</p>
       </Panel>
 
-      <Panel header="How long will my order take to get to me?" key="2">
+      <Panel header="How long will my order take to get to me?" key="4">
         <p>Orders usually take 1 to 2 days to get to the recipient.</p>
       </Panel>
 
-      <Panel header="Can i cancel an order that is already shipped?" key="3">
+      <Panel header="Can i cancel an order that is already shipped?" key="5">
         <p>If the order has already been shipped and is on its way to you, it may not be possible to cancel it. 
           However, you can contact the seller and ask if they can cancel the order or if they offer any options 
           for returning or exchanging the item once it has been received. </p>
       </Panel>
 
-      <Panel header="Can i receive a call before delivery?" key="4">
+      <Panel header="Can i receive a call before delivery?" key="6">
         <p>It depends on the delivery service and the seller's policy. 
           Some delivery services offer the option to receive a call before delivery, while others do not. 
           You can contact the seller and ask if they offer this service or if the delivery service they use provides it.  </p>
       </Panel>
 
-      <Panel header="What if I’m not available when my order arrives?" key="5">
+      <Panel header="What if I’m not available when my order arrives?" key="7">
         <p>If you're not available when your order arrives you can contact a someone that can receive the order in your behalf. </p>
       </Panel>
 
-      <Panel header="Can i change my delivery address after placing my order?" key="6">
+      <Panel header="Can i change my delivery address after placing my order?" key="8">
         <p>Whether you can change your delivery address after placing an order depends on the seller and the shipping status of the order. 
           If the order has not yet been shipped, you may be able to change the delivery address by contacting the seller. 
           If the order has already been shipped, it may not be possible to change the delivery address. </p>
       </Panel>
 
-      <Panel header="Can i choose the courier who will deliver my order?" key="7">
+      <Panel header="Can i choose the courier who will deliver my order?" key="9">
         <p>You can check with the seller to see what shipping options they offer and if you have the ability to choose the courier for your delivery. 
           If the seller does not offer the option to choose a courier, 
           they may be able to provide more information on the courier that will be used for your delivery and the estimated delivery time. </p>
       </Panel>
 
-      <Panel header="Can i select the vehicle that will be use to ship my order?" key="8">
+      <Panel header="Can i select the vehicle that will be use to ship my order?" key="10">
         <p>This is typically handled by the courier service and is based on factors such as the size and weight of the shipment, 
           the delivery route, and the available vehicles in the area. The courier service will use the most efficient and cost-effective method to deliver your order, 
           which may not involve a specific type of vehicle. If you have specific delivery requirements, such as a preference for a particular type of vehicle, 
--- src/pages/help/Help.jsx
+++ src/pages/help/Help.jsx
@@ -1,63 +1,105 @@
 import Page from "@/components/Page";
 import PageHeader from "@/components/PageHeader";
-import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { Avatar, Input } from "antd";
 
+export default function Help() {
+  return (
+    <Page>
+      <PageHeader title="TalipaAPP Help Center" back="/"/>
+      <div className="my-2 bg-white">
+        <div className="mx-4 flex grow pt-2">
+          <Input
+            type="text"
+            className="rounded-lg "
+            placeholder="Search for topics, questions..."
+            suffix={<SearchOutlined />}
+          />
+        </div>
 
-export default function Help()
-{
-    return(
-        <Page>
-              <PageHeader title="TalipaAPP Help Center" />
-              <div className="flex">
-                <div className="flex grow pt-2">
-                <Input type="text" className="rounded-lg " /> 
-                </div>
-              </div>
+        <div className="mx-4 flex grow flex-col p-2">
+          <div className=" text-xl font-bold">
+            <p>Self-Service</p>
+          </div>
 
-              <div className="flex">
-              <div className="flex grow font-bold p-2 text-xl">
-                    <p>Self-Service</p>
-                </div>
-              </div>
+          <div className="flex-cols-2 mx-4 flex items-center gap-4 pb-4 font-bold">
+            <img src="/assets/images/track.png" className="h-20 w-20 " />
+            <span>Track Order</span>
+            <img src="/assets/images/changepass.png" className="h-20 w-20 " />
+            <span>Track Order</span>
+          </div>
+          <div className="flex-cols-2 mx-4 flex items-center gap-4 font-bold">
+            <img src="/assets/images/changenumber.png" className="h-20 w-20 " />
+            <span>Track Order</span>
+            <img src="/assets/images/delete.png" className="h-20 w-20 " />
+            <span>Track Order</span>
+          </div>
+        </div>
+      </div>
 
-              <div className="flex">
-              <div className="flex grow font-bold p-2 text-xl">
-                    <p>Hot Questions</p>
-                </div>
-              </div>
-                <div className="flex grow pl-2">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+      <div className=" bg-white">
+        <div className="flex grow p-2 text-xl font-bold">
+          <p>Hot Questions</p>
+        </div>
+        <div className="mx-4 pb-2">
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
 
-              <div className="flex grow pl-2">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
 
-                <div className="flex grow pl-2">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
 
-                <div className="flex grow pl-2">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
 
-                <div className="flex grow pl-2">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
 
-                <div className="flex grow pl-2">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
 
-              <div className="flex">
-              <div className="flex grow font-bold p-2 text-xl">
-                    <p>Do You Have Any Other Question?</p>
-                </div>
-              </div>
+      <div className="my-2 bg-white">
+        <div className="flex grow p-2 text-xl font-bold">
+          <p>Do You Have Any Other Question?</p>
+        </div>
+      </div>
 
-              <div className="flex grow pl-2">
-                    <p>Chat</p>
-                </div>
-        </Page>
-    );
+      <div className="my-2 bg-white">
+        <div className="my-4 flex">
+          <div className="flex items-center">
+            <Avatar className="m-2 " />
+            <p className="text-lg"> Chat</p>
+          </div>
+        </div>
+      </div>
+    </Page>
+  );
 }
