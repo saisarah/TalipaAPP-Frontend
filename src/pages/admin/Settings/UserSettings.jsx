e97fe3abb23a17cc8dd1def46c14abfc65f35b50
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-02-09 00:40:14 +0800

    settings-change-pass-designed

---
+++ src/pages/admin/Settings/UserSettings.jsx
@@ -0,0 +1,66 @@
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Button, Input, Space } from "antd";
+
+export default function UserSettings() {
+  const onChange = (e) => {
+    console.log(e);
+  };
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <div className="flex flex-1 flex-col p-40">
+          <p className="text-3xl font-bold text-primary">
+            Talipaapp Accout Change password
+          </p>
+          <p className="text-lg">
+            We recommend that you periodically update your password to help to
+            prevent unauthorized access to your account.
+          </p>
+        </div>
+        <div className="flex-1 gap-5 p-40">
+          <div className="pt-2">
+            <Input placeholder="USERNAME" allowClear onChange={onChange} />
+          </div>
+          <br />
+          <div className="pt-2">
+            <Input.Password
+              placeholder="CURRENT PASSWORD"
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </div>
+          <br />
+          <div className="pt-2">
+            <Input.Password
+              placeholder="NEW PASSWORD"
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </div>
+          <br />
+          <div className="pt-2">
+            <Input.Password
+              placeholder="NEW PASSWORD"
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </div>
+          <div className="flex grow gap-6 p-10">
+            <Button className="rounded px-20 font-bold" size="large">
+              CANCEL
+            </Button>
+            <Button
+              className="rounded bg-primary px-20 font-bold text-white"
+              size="large"
+            >
+              SAVE CHANGES
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,18 +1,21 @@
 import AdminLayout from "@/pages/admin/AdminLayout/AdminLayout";
 import Dashboard from "@/pages/admin/Dashboard/Dashboard";
 import Login from "@/pages/admin/Login/Login";
+import UserSettings from "@/pages/admin/Settings/Usersettings";
 
 export default [
   {
     element: <AdminLayout />,
     children: [
-        {
-            path: "",
-            element: <Dashboard/>,
-        },
-        
-        
-    ]
+      {
+        path: "",
+        element: <Dashboard />,
+      },
+      {
+        path: "usersettings",
+        element: <UserSettings />,
+      },
+    ],
   },
   {
     path: "login",
