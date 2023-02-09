ebad48ec47e2f904e4c348c0591a426d2127cc70
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-02-09 18:53:07 +0800

    user management vendor list designed

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,18 +1,21 @@
 import AdminLayout from "@/pages/admin/AdminLayout/AdminLayout";
 import Dashboard from "@/pages/admin/Dashboard/Dashboard";
 import Login from "@/pages/admin/Login/Login";
+import ManagementVendor from "@/pages/admin/UserManagement/ManagementVendor";
 
 export default [
   {
     element: <AdminLayout />,
     children: [
-        {
-            path: "",
-            element: <Dashboard/>,
-        },
-        
-        
-    ]
+      {
+        path: "",
+        element: <Dashboard />,
+      },
+      {
+        path: "vendor",
+        element: <ManagementVendor />,
+      },
+    ],
   },
   {
     path: "login",
