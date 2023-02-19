7aed14a41213f59259f606cb7a132ceda515ff9d
Josh <villanuevajoshua27@gmail.com>
2023-02-19 12:00:16 +0800

    table vendor fixed

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -6,7 +6,7 @@ import Dashboard from "@/pages/admin/Dashboard/Dashboard";
 import Login from "@/pages/admin/Login/Login";
 import FarmerGroup from "@/pages/admin/UserManagement/FarmerGroup";
 import Farmer from "@/pages/admin/UserManagement/Farmer";
-import ManagementVendor from "@/pages/admin/UserManagement/ManagementVendor";
+import Vendor from "@/pages/admin/UserManagement/Vendor";
 
 export default [
   {
@@ -36,11 +36,11 @@ export default [
         path: "farmers",
         element: <Farmer />,
       },
+      {
+        path: "vendors",
+        element: <Vendor />,
+      },
     ],
-  },
-  {
-    path: "vendors",
-    element: <ManagementVendor />,
   },
 
   {
