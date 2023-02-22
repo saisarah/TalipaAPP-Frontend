d36f554bc6545c9729fc30d2e441b6797aad4955
Uno-count <paonemorfi@gmail.com>
2023-02-22 08:38:51 +0800

    Add fetchMessages function and useQuery hook to PrivateMessage componentAdd fetchMessages function and useQuery hook to PrivateMessage component for retrieving messages from API

--- src/pages/farmer/Chat/PrivateMessage.jsx
+++ src/pages/farmer/Chat/PrivateMessage.jsx
@@ -11,9 +11,23 @@ import {
   ShoppingCartOutlined,
 } from "@ant-design/icons";
 import Meta from "antd/lib/card/Meta";
+import Http from "@/helpers/Http";
+import { useParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchMessages = async (id) => {
+  const result = await Http.get(`/messages/${id}`);
+  return result.data;
+};
 
 // test AICommits
 export default function PrivateMessage() {
+  const params = useParams();
+  const { data, isLoading } = useQuery(["messages", params.id], () =>
+    fetchMessages(params.id)
+  );
+  if (isLoading) return "Loading...";
+
   const App = () => <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
   const onSearch = (value) => console.log(value);
   const suffix = (
@@ -26,77 +40,51 @@ export default function PrivateMessage() {
   );
   // <Avatar size="small" style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />,
   return (
-    <div className="app-size bg-white">
+    <div className="app-size flex flex-col bg-white">
       <PageHeader back="/farmer/chat" title="Commonwealt Market Quezon City." />
-
       <div className="flex p-2">
         <div className="flex grow justify-center bg-white p-2" size="large">
           <span>10:00</span>
         </div>
       </div>
-      <div className=" flex items-end gap-2 p-4">
-        <Avatar
-          size="medium"
-          style={{ backgroundColor: "#87d068" }}
-          icon={<UserOutlined />}
-        />
-        <Card
-          className="p-3 shadow-sm"
-          style={{
-            width: 250,
-          }}
-          cover={<img alt="example" src="/assets/images/garlic.png" />}
-          actions={[
-            <ShoppingCartOutlined key="ShoppingCart" />,
-            <DeleteOutlined key="Delete" />,
-            <EllipsisOutlined key="ellipsis" />,
-          ]}
-        >
-          <Meta title="Garlic" description="Price: 750.00 Pesos" />
-        </Card>
+      <div className="flex flex-1 flex-col-reverse">
+        <div className="mb-4">
+          {data.map((item) =>
+            params.id == item.receiver_id ? (
+              <div key={item.id} className="row-end-1 flex justify-end p-4">
+                <Card
+                  className="row-end-1 shadow-sm"
+                  style={{
+                    width: 300,
+                  }}
+                >
+                  <p>{item.content}</p>
+                </Card>
+              </div>
+            ) : (
+              <div
+                key={item.id}
+                className="flex items-end justify-start gap-2 p-4"
+              >
+                <Avatar
+                  size="medium"
+                  style={{ backgroundColor: "#87d068" }}
+                  icon={<UserOutlined />}
+                />
+                <Card
+                  className="shadow-sm"
+                  style={{
+                    width: 300,
+                  }}
+                >
+                  <p>{item.content}</p>
+                </Card>
+              </div>
+            )
+          )}
+        </div>
       </div>
-      <div className="row-end-1 flex justify-end p-4">
-        <Card
-          className="row-end-1 shadow-sm"
-          style={{
-            width: 300,
-          }}
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-            porta massa sapien. Suspendisse iaculis tellus lacus, ac facilisis
-            massa maximus vitae. Proin posuere nec nisl eu eleifend. Mauris
-            imperdiet egestas posuere. Aenean at iaculis nunc. Suspendisse
-            luctus dolor sed nulla elementum ultrices. Orci varius natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Morbi sit amet tortor vel orci rutrum condimentum iaculis eget est.
-          </p>
-        </Card>
-      </div>
-      <div className="justify-s flex items-end gap-2 p-4">
-        <Avatar
-          size="medium"
-          style={{ backgroundColor: "#87d068" }}
-          icon={<UserOutlined />}
-        />
-        <Card
-          className="shadow-sm"
-          style={{
-            width: 300,
-          }}
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-            porta massa sapien. Suspendisse iaculis tellus lacus, ac facilisis
-            massa maximus vitae. Proin posuere nec nisl eu eleifend. Mauris
-            imperdiet egestas posuere. Aenean at iaculis nunc. Suspendisse
-            luctus dolor sed nulla elementum ultrices. Orci varius natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Morbi sit amet tortor vel orci rutrum condimentum iaculis eget est.
-          </p>
-        </Card>
-      </div>
-      <div className="sticky bottom-2 bg-white pl-2">
+      <div className="sticky bottom-2 left-0 right-0 bg-white pl-2">
         <div className="">
           <Input.Group className="">
             <Input
