fd4c3b5364bac8748bc3f632a62c97e2c0bc83ff
Uno-count <paonemorfi@gmail.com>
2023-02-22 12:41:51 +0800

    Update Chat component to include ChatItem component and fetch messages from the backend

--- src/pages/vendor/Messages/Chat.jsx
+++ src/pages/vendor/Messages/Chat.jsx
@@ -1,82 +1,26 @@
 import Page from "@/components/Page";
-import { Avatar, Badge, Space } from "antd";
-import { Link } from "react-router-dom";
+import PageHeader from "@/components/PageHeader";
+import Http from "@/helpers/Http";
+import { useQuery } from "@tanstack/react-query";
 import { useTitle } from "../components/VendorLayout/VendorLayout";
+import ChatItem from "./components/ChatItem";
+
+const fetchMessages = async () => {
+  const result = await Http.get("/messages");
+  return result.data;
+};
 
 export default function Chat() {
   useTitle("Chat");
+  const { data, isLoading } = useQuery(["messages"], fetchMessages);
+
+  if (isLoading) return "Loading...";
+
   return (
     <Page className="bg-gray-100">
-      <Link to="/messages/1" className="mx-2 flex pt-1">
-        <div className="flex grow flex-row rounded-md bg-white shadow-sm">
-          <div className="flex justify-center p-4">
-            <Space size="middle">
-              <Badge className="" count={7}>
-                <Avatar
-                  className="border border-green-500"
-                  shape="rounded"
-                  size="large"
-                />
-              </Badge>
-            </Space>
-            <div className="flex flex-col p-2 text-xs">
-              <span className="ml-2 font-bold">
-                {" "}
-                Bagong Silang Market Caloocan City.
-              </span>
-              <span className="ml-2"> 8:14 am</span>
-              <span className="ml-2">[Order Confirmation]</span>
-            </div>
-          </div>
-        </div>
-      </Link>
-
-      <div className="mx-2 flex pt-1">
-        <div className="flex grow flex-row rounded-md bg-white shadow-sm">
-          <div className="flex justify-center p-4">
-            <Space size="middle">
-              <Badge className="" count={0}>
-                <Avatar
-                  className="border border-green-500"
-                  shape="rounded"
-                  size="large"
-                />
-              </Badge>
-            </Space>
-            <div className="flex flex-col p-2 text-xs">
-              <span className="ml-2 font-bold">
-                {" "}
-                Commonwealt Market Quezon City.
-              </span>
-              <span className="ml-2"> 12 hours</span>
-              <span className="ml-2">[Order Confirmation]</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="mx-2 flex pt-1">
-        <div className="flex grow flex-row rounded-md bg-white shadow-sm">
-          <div className="flex justify-center p-4">
-            <Space size="middle">
-              <Badge className="" count={1}>
-                <Avatar
-                  className="border border-green-500"
-                  shape="rounded"
-                  size="large"
-                />
-              </Badge>
-            </Space>
-            <div className="flex flex-col p-2 text-xs">
-              <span className="ml-2 font-bold">
-                {" "}
-                Vetable Farm Maycauyan bulacan.
-              </span>
-              <span className="ml-2"> Yesterday</span>
-              <span className="ml-2">On delivery na sir</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      {data.map((item) => (
+        <ChatItem key={item.id} {...item} />
+      ))}
     </Page>
   );
 }
