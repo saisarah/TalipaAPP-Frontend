280ebb49317fba9735d5b697611d84947c8952f6
lenard123 <lenard.mangayayam@gmail.com>
2023-02-28 20:19:26 +0000

    Update login mutation to display success/error notifications

--- src/query/mutations/useLogin.js
+++ src/query/mutations/useLogin.js
@@ -3,13 +3,16 @@ import { currentUserKey } from "@/apis/UserApi";
 import { setAuthorization } from "@/helpers/Http";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export const useLogin = function () {
+export const useLogin = function ({ onSuccess, onError }) {
   const queryClient = useQueryClient();
 
   return useMutation(login, {
     onSuccess({ token, user }) {
       setAuthorization(token);
       queryClient.setQueryData(currentUserKey, user);
+      if (onSuccess) onSuccess({ token, user });
     },
+
+    onError,
   });
 };
