556aee497bd950969a058202eb0795a950e08fb6
r-kyle <kylerubante@gmail.com>
2023-03-04 15:35:36 +0800

    Help Routes

--- src/routes/guestRoutes.jsx
+++ src/routes/guestRoutes.jsx
@@ -4,6 +4,8 @@ import Login from "../pages/auth/login/Login";
 import Register from "../pages/auth/register/Register";
 import Faq from "@/pages/help/Faq";
 import Help from "@/pages/help/Help";
+import Help1 from "@/pages/help/item/help1"
+import help2 from "@/pages/help/item/help2"
 
 export default [
   {
@@ -25,5 +27,13 @@ export default [
   {
     path: "/help",
     element: <Help />,
+  },
+  {
+    path: "/help/item/help1",
+    element: <Help1 />,
+  },
+  {
+    path: "/help/item/help2",
+    element: <help2 />,
   }
 ];
