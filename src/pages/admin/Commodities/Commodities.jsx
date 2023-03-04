6e1e9a751aae470c48e79d2cd5394171e97d9237
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2023-03-04 23:09:45 +0800

    commodities-revision-updated

--- src/pages/admin/Commodities/Commodities.jsx
+++ src/pages/admin/Commodities/Commodities.jsx
@@ -1,3 +1,5 @@
+import FormItem from "@/components/FormItem";
+import { MenuOutlined } from "@ant-design/icons";
 import {
   Row,
   Col,
@@ -8,15 +10,34 @@ import {
   Button,
   Avatar,
   Typography,
+  Dropdown,
+  Modal,
+  Form,
+  Input,
+  Space,
+  Select,
 } from "antd";
+import React, { useState } from "react";
+import { rules } from "./rules";
 
 // Images
-import face from "/assets/images/banana-com.png";
-import face2 from "/assets/images/garlic-com.png";
-import face3 from "/assets/images/eggplant-com.png";
-import face4 from "/assets/images/cabbage-com.png";
-import face5 from "/assets/images/onion-com.png";
-import face6 from "/assets/images/pineapple-com.png";
+// import face from "/assets/images/banana-com.png";
+// import face2 from "/assets/images/garlic-com.png";
+// import face3 from "/assets/images/eggplant-com.png";
+// import face4 from "/assets/images/cabbage-com.png";
+// import face5 from "/assets/images/onion-com.png";
+// import face6 from "/assets/images/pineapple-com.png";
+
+const items = [
+  {
+    key: "1",
+    label: "Edit",
+  },
+  {
+    key: "2",
+    label: "Delete",
+  },
+];
 
 const { Title } = Typography;
 
@@ -40,21 +61,26 @@ const formProps = {
 // table code start
 const columns = [
   {
-    title: "COMMODITIES",
+    title: "Commodities",
     dataIndex: "name",
     key: "name",
     width: "32%",
   },
   {
-    title: "PLACE HARVESTED",
-    dataIndex: "function",
-    key: "function",
+    title: "Number of Posts",
+    dataIndex: "posts",
+    key: "posts",
   },
 
   {
-    title: "STATUS",
-    key: "status",
-    dataIndex: "status",
+    title: "Number of Demands",
+    key: "demands",
+    dataIndex: "demands",
+  },
+  {
+    title: "Actions",
+    key: "actions",
+    dataIndex: "actions",
   },
 ];
 
@@ -63,34 +89,37 @@ const data = [
     key: "1",
     name: (
       <>
-        <Avatar.Group>
-          <Avatar
-            className="shape-avatar mr-2"
-            shape="square"
-            size={40}
-            src={face}
-          ></Avatar>
-          <div className="avatar-info">
-            <Title level={5}>Banana</Title>
-            <p>Description</p>
-          </div>
+        <Avatar.Group
+          // shape="square"
+          size={40}
+          // src={face}
+        >
+          <Title level={4}>Banana</Title>
         </Avatar.Group>{" "}
       </>
     ),
-    function: (
+    posts: (
       <>
-        <div className="author-info">
-          <Title level={5}>Mindanao</Title>
-          <p>Organization</p>
-        </div>
+        <Title level={4}>54</Title>
       </>
     ),
 
-    status: (
+    demands: (
       <>
-        <Button type="primary" className="tag-primary">
-          AVAILABLE
-        </Button>
+        <Title level={4}>63</Title>
+      </>
+    ),
+    actions: (
+      <>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a>
+            <MenuOutlined />
+          </a>
+        </Dropdown>
       </>
     ),
   },
@@ -99,32 +128,37 @@ const data = [
     key: "2",
     name: (
       <>
-        <Avatar.Group>
-          <Avatar
-            className="shape-avatar mr-2"
-            shape="square"
-            size={40}
-            src={face2}
-          ></Avatar>
-          <div className="avatar-info">
-            <Title level={5}>Garlic</Title>
-            <p>Description</p>
-          </div>
+        <Avatar.Group
+          // shape="square"
+          size={40}
+          // src={face2}
+        >
+          <Title level={4}>Garlic</Title>
         </Avatar.Group>{" "}
       </>
     ),
-    function: (
+    posts: (
       <>
-        <div className="author-info">
-          <Title level={5}>Luzon</Title>
-          <p>Organization</p>
-        </div>
+        <Title level={4}>108</Title>
       </>
     ),
 
-    status: (
+    demands: (
       <>
-        <Button className="tag-badge">AVAILABLE</Button>
+        <Title level={4}>99</Title>
+      </>
+    ),
+    actions: (
+      <>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a>
+            <MenuOutlined />
+          </a>
+        </Dropdown>
       </>
     ),
   },
@@ -133,34 +167,37 @@ const data = [
     key: "3",
     name: (
       <>
-        <Avatar.Group>
-          <Avatar
-            className="shape-avatar mr-2"
-            shape="square"
-            size={40}
-            src={face3}
-          ></Avatar>
-          <div className="avatar-info">
-            <Title level={5}>Eggplant</Title>
-            <p>Description</p>
-          </div>
+        <Avatar.Group
+          // shape="square"
+          size={40}
+          // src={face3}
+        >
+          <Title level={4}>Eggplant</Title>
         </Avatar.Group>{" "}
       </>
     ),
-    function: (
+    posts: (
       <>
-        <div className="author-info">
-          <Title level={5}>Visayas</Title>
-          <p>Organization</p>
-        </div>
+        <Title level={4}>97</Title>
       </>
     ),
 
-    status: (
+    demands: (
       <>
-        <Button type="primary" className="tag-primary">
-          AVAILABLE
-        </Button>
+        <Title level={4}>56</Title>
+      </>
+    ),
+    actions: (
+      <>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a>
+            <MenuOutlined />
+          </a>
+        </Dropdown>
       </>
     ),
   },
@@ -168,34 +205,37 @@ const data = [
     key: "4",
     name: (
       <>
-        <Avatar.Group>
-          <Avatar
-            className="shape-avatar mr-2"
-            shape="square"
-            size={40}
-            src={face4}
-          ></Avatar>
-          <div className="avatar-info">
-            <Title level={5}>Cabbage</Title>
-            <p>Description</p>
-          </div>
+        <Avatar.Group
+          // shape="square"
+          size={40}
+          // src={face4}
+        >
+          <Title level={4}>Cabbage</Title>
         </Avatar.Group>{" "}
       </>
     ),
-    function: (
+    posts: (
       <>
-        <div className="author-info">
-          <Title level={5}>Visayas</Title>
-          <p>Organization</p>
-        </div>
+        <Title level={4}>88</Title>
       </>
     ),
 
-    status: (
+    demands: (
       <>
-        <Button type="primary" className="tag-primary">
-          AVAILABLE
-        </Button>
+        <Title level={4}>105</Title>
+      </>
+    ),
+    actions: (
+      <>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a>
+            <MenuOutlined />
+          </a>
+        </Dropdown>
       </>
     ),
   },
@@ -203,32 +243,37 @@ const data = [
     key: "5",
     name: (
       <>
-        <Avatar.Group>
-          <Avatar
-            className="shape-avatar mr-2"
-            shape="square"
-            size={40}
-            src={face5}
-          ></Avatar>
-          <div className="avatar-info">
-            <Title level={5}>Onion</Title>
-            <p>Description</p>
-          </div>
+        <Avatar.Group
+          // shape="square"
+          size={40}
+          // src={face5}
+        >
+          <Title level={4}>Onion</Title>
         </Avatar.Group>{" "}
       </>
     ),
-    function: (
+    posts: (
       <>
-        <div className="author-info">
-          <Title level={5}>Mindanao</Title>
-          <p>Organization</p>
-        </div>
+        <Title level={4}>110</Title>
       </>
     ),
 
-    status: (
+    demands: (
       <>
-        <Button className="tag-badge">AVAILABLE</Button>
+        <Title level={4}>91</Title>
+      </>
+    ),
+    actions: (
+      <>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a>
+            <MenuOutlined />
+          </a>
+        </Dropdown>
       </>
     ),
   },
@@ -237,120 +282,107 @@ const data = [
     key: "6",
     name: (
       <>
-        <Avatar.Group>
-          <Avatar
-            className="shape-avatar mr-2"
-            shape="square"
-            size={40}
-            src={face6}
-          ></Avatar>
-          <div className="avatar-info">
-            <Title level={5}>Pineapple</Title>
-            <p>Description</p>
-          </div>
+        <Avatar.Group
+          // shape="square"
+          size={40}
+          // src={face6}
+        >
+          <Title level={4}>Pineapple</Title>
         </Avatar.Group>{" "}
       </>
     ),
-    function: (
+    posts: (
       <>
-        <div className="author-info">
-          <Title level={5}>Mindanao</Title>
-          <p>Organization</p>
-        </div>
+        <Title level={4}>41</Title>
       </>
     ),
 
-    status: (
+    demands: (
       <>
-        <Button className="tag-badge">AVAIABLE</Button>
+        <Title level={4}>87</Title>
+      </>
+    ),
+    actions: (
+      <>
+        {/* <FormItem name="action" placeholder="Actions" className="w-24">
+          <Select size="large"  placeholder="Actions">
+            <Select.Option value="edit">Edit</Select.Option>
+            <Select.Option value="delete">Delete</Select.Option>
+          </Select>
+        </FormItem> */}
+
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a>
+            <MenuOutlined />
+          </a>
+        </Dropdown>
       </>
     ),
   },
 ];
 
-function Product({ src, price, title }) {
-  return (
-    <>
-      <div className="flex flex-col">
-        <img
-          src={src}
-          alt="farmer's-profile"
-          className="h-56 w-48 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
-        />
-        <div className="my-2 flex flex-col text-center">
-          <span>{title}</span>
-          <span className="text-primary">{price}</span>
-        </div>
-      </div>
-    </>
-  );
-}
+// function Product({ src, price, title }) {
+//   return (
+//     <>
+//       <div className="flex flex-col">
+//         <img
+//           src={src}
+//           alt="farmer's-profile"
+//           className="h-56 w-48 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+//         />
+//         <div className="my-2 flex flex-col text-center">
+//           <span>{title}</span>
+//           <span className="text-primary">{price}</span>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 
 export default function Commodities() {
-  const onChange = (e) => console.log(`radio checked:${e.target.value}`);
+  const handleSubmit = (personalData) => {
+    setData((data) => ({ ...data, ...personalData }));
+    setStep((step) => step + 1);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
+      <div>
+        <Modal
+          title="Add Commodities"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <Form initialValues={data} layout="vertical" onFinish={handleSubmit}>
+            <FormItem
+              name="name"
+              rules={rules.name}
+              placeholder="Enter your name here"
+            />
+          </Form>
+        </Modal>
+      </div>
       <div className="my-2 text-center text-xl text-primary-accent-3">
         <span>Featured Commodities</span>
-      </div>
-      <div className="text-center text-4xl">
-        <h1>Our Commodities</h1>
-      </div>
-      <div className="mx-52 my-16 mt-10 grid grid-cols-4 items-center gap-4">
-        <div>
-          <Card className="flex h-80 w-64 justify-center bg-slate-50 text-lg">
-            <Product
-              src={"/assets/images/banana-com.png"}
-              price={"₱120.00"}
-              title={"Banana"}
-            />
-          </Card>
-        </div>
-        <div className="flex items-center">
-          <Card className="flex h-80 w-64 justify-center bg-slate-50 text-lg ">
-            <Product
-              src={"/assets/images/garlic-com.png"}
-              price={"₱80.00"}
-              title={"Garlic"}
-            />
-          </Card>
-        </div>
-        <div>
-          <Card className="flex h-80 w-64 justify-center bg-slate-50 text-lg ">
-            <Product
-              src={"/assets/images/eggplant-com.png"}
-              price={"₱100.00"}
-              title={"Eggplant"}
-            />
-          </Card>
-        </div>
-        <div>
-          <Card className="flex h-80 w-64 justify-center bg-slate-50 text-lg ">
-            <Product
-              src={"/assets/images/cabbage-com.png"}
-              price={"₱150.00"}
-              title={"Cabbage"}
-            />
-          </Card>
-        </div>
-        <div>
-          <Card className="flex h-80 w-64 justify-center bg-slate-50 text-lg">
-            <Product
-              src={"/assets/images/onion-com.png"}
-              price={"₱450.00"}
-              title={"Onion"}
-            />
-          </Card>
-        </div>
-        <div>
-          <Card className="flex h-80 w-64 justify-center bg-slate-50 text-lg">
-            <Product
-              src={"/assets/images/pineapple-com.png"}
-              price={"₱120.00"}
-              title={"Pineapple"}
-            />
-          </Card>
-        </div>
       </div>
       <div className="tabled mx-52">
         <Row gutter={[24, 0]}>
@@ -361,10 +393,9 @@ export default function Commodities() {
               title="Commodities Table"
               extra={
                 <>
-                  <Radio.Group onChange={onChange} defaultValue="a">
-                    <Radio.Button value="a">All</Radio.Button>
-                    <Radio.Button value="b">Available</Radio.Button>
-                  </Radio.Group>
+                  <Button type="primary" onClick={showModal}>
+                    Add Commodities
+                  </Button>
                 </>
               }
             >
