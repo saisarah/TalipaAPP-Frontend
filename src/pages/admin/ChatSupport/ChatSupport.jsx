5b9ad9896c4ebf3c9b7d8da546889f1f4e8120d0
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2023-03-04 14:44:12 +0800

    Co-authored-by: Lenard  Mangay-ayam <lenard123@users.noreply.github.com>

--- src/pages/admin/ChatSupport/ChatSupport.jsx
+++ src/pages/admin/ChatSupport/ChatSupport.jsx
@@ -5,14 +5,12 @@ import React from "react";
 function User({ name, chat, email, time }) {
   return (
     <>
-      <div>
-        <div className="flex">
-          <Avatar className="h-10 w-10" />
-          <div className="ml-2 flex flex-col">
-            <span className="font-bold">{name}</span>
-            <span className="leading-tight text-slate-400">{email}</span>
-            <p className="whitespace-nowrap">{chat}</p>
-          </div>
+      <div className="flex">
+        <Avatar className="h-10 w-10" />
+        <div className="ml-2 flex flex-col">
+          <span className="font-bold">{name}</span>
+          <span className="leading-tight text-slate-400">{email}</span>
+          <span className="whitespace-nowrap">{chat}</span>
         </div>
       </div>
     </>
@@ -22,8 +20,8 @@ function User({ name, chat, email, time }) {
 export default function ChatSupport() {
   const onSearch = (value) => console.log(value);
   return (
-    <div>
-      <div className="flex">
+    <div className="flex">
+      <div className="flex flex-none">
         <Card>
           <div className=" border-slate-400">
             <div className="m-2 w-auto">
@@ -52,24 +50,23 @@ export default function ChatSupport() {
             </div>
           </div>
         </Card>
-        <div className="flex justify-center border-slate-400 ml-2">
-            <div className="flex w-full">
-              <Card>
-                <div className="my-2 ">
-                  <span className="border-b-2 font-bold text-lg pb-2">Juan Paolo Ortega</span>
-                </div>
-
-                <div className="my-2 items-center">
-                  <Avatar className="h-10 w-10 mt-2"/>
-                  <span className="items-center mx-2"> Hello there brother!</span>
-                </div>
-                <div className="flex flex-row-reverse items-center">
-                <Avatar className="h-10 w-10 mt-2"/>
-                  <span className="items-center mx-2"> Hello there brother!</span>
-                </div>
-              </Card>
+      </div>
+      <div className="ml-2 flex w-full grow border-slate-400">
+        <Card>
+          <div className="my-2 ">
+            <span className="border-b-2 pb-2 text-lg font-bold">
+              Juan Paolo Ortega
+            </span>
           </div>
-        </div>
+          <div className="my-2 items-center">
+            <Avatar className="mt-2 h-10 w-10" />
+            <span className="mx-2 items-center"> Hello there brother!</span>
+          </div>
+          <div className="flex flex-row-reverse items-center">
+            <Avatar className="mt-2 h-10 w-10" />
+            <span className="mx-2 items-center"> Hello there brother!</span>
+          </div>
+        </Card>
       </div>
     </div>
   );
