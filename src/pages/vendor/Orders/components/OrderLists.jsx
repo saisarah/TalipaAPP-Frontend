2bb73d15aee5eaee327220847a4aaa6de1f31b5b
Uno-count <paonemorfi@gmail.com>
2023-03-09 00:44:21 +0800

    add orders empty state

--- src/pages/vendor/Orders/components/OrderLists.jsx
+++ src/pages/vendor/Orders/components/OrderLists.jsx
@@ -2,7 +2,7 @@ import { fetchOrders } from "@/apis/OrderApi";
 import OrderItem from "@/components/OrderItem/OrderItem";
 import { useQuery } from "@tanstack/react-query";
 import { Spin } from "antd";
-
+import emptySvg from "../image/undraw_empty_cart_co35.svg";
 export default function OrderLists({ status }) {
   const { data, isLoading } = useQuery(
     fetchOrders.key(status),
@@ -13,6 +13,16 @@ export default function OrderLists({ status }) {
     return (
       <div className="flex flex-col items-center py-16">
         <Spin />
+      </div>
+    );
+
+  if (data.length == 0)
+    return (
+      <div className="flex flex-col items-center py-40 ">
+        <img width="200" height="100" src={emptySvg} alt="" />
+        <span className="px-24 text-center text-sm">
+          You have no orders yet, browse commodities in the home section
+        </span>
       </div>
     );
 
