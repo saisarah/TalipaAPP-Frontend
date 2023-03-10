b291ee89791c1f000de8802e12eda12e261923d5
lenard123 <lenard.mangayayam@gmail.com>
2023-03-11 04:51:28 +0800

    }Refactor ChatItem component and improve conversation scrolling experience

--- src/pages/farmer/Messages/Chat.jsx
+++ src/pages/farmer/Messages/Chat.jsx
@@ -2,28 +2,13 @@ import Conversation from "@/components/messages/Conversation";
 import SendMessage from "@/components/messages/SendMessage";
 import Page from "@/components/Page";
 import PageHeader from "@/components/PageHeader";
-import { useConversationsQuery } from "@/query/queries/useMessagesQuery";
 import { useUserQuery } from "@/query/queries/useUsersQuery";
-import { Spin } from "antd";
 import SkeletonInput from "antd/lib/skeleton/Input";
 import { useParams } from "react-router";
 
 export default function Chat() {
   const { id } = useParams();
-  const { data, isLoading } = useConversationsQuery(id);
   const { data: user, isLoading: isUserLoading } = useUserQuery(id);
-
-  console.log({ data, isLoading });
-  // if (isLoading || isUserLoading)
-  //   return (
-  //     <Page>
-  //       <PageHeader back="/farmer/chat" title="Chat" />
-  //       <div className="flex flex-col items-center py-16">
-  //         <Spin />
-  //       </div>
-  //     </Page>
-  //   );
-  // if (isLoading) return null;
 
   return (
     <Page className="flex h-full max-h-screen flex-col bg-white">
@@ -31,14 +16,7 @@ export default function Chat() {
         back="/farmer/messages"
         title={isUserLoading ? <SkeletonInput /> : user.fullname}
       />
-      {isLoading ? (
-        <Spin
-          className="flex flex-grow items-center justify-center"
-          tip="Fetching messages"
-        />
-      ) : (
-        <Conversation messages={data} user={user} />
-      )}
+      <Conversation id={id} avatar={user?.profile_picture} />
       <SendMessage id={id} />
     </Page>
   );
