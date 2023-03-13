ee38e1e1e59455d2bf57936a32a18f42a5b48f42
Uno-count <paonemorfi@gmail.com>
2023-03-13 12:00:26 +0800

    WIP

--- src/components/Demands/post-data.jsx
+++ src/components/Demands/post-data.jsx
@@ -1,121 +1,170 @@
-import { Link } from "react-router-dom";
-
-// START COMMODITY DATA
-export const dataCommodities = [
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Input, Space, Table } from "antd";
+import { useRef, useState } from "react";
+import Highlighter from "react-highlight-words";
+const data = [
   {
     key: "1",
-    commodities: "Pineapple",
-    percentage: "0%",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
   },
   {
     key: "2",
-    commodities: "Mango",
-    percentage: "0%",
+    name: "Joe Black",
+    age: 42,
+    address: "London No. 1 Lake Park",
   },
   {
     key: "3",
-    commodities: "Banana",
-    percentage: "100%",
+    name: "Jim Green",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
   },
   {
     key: "4",
-    commodities: "Garlic",
-    percentage: "0%",
-  },
-  {
-    key: "5",
-    commodities: "Onion",
-    percentage: "0%",
-  },
-  {
-    key: "6",
-    commodities: "Cabbage",
-    percentage: "0%",
-  },
-  {
-    key: "7",
-    commodities: "Eggplant",
-    percentage: "0%",
+    name: "Jim Red",
+    age: 32,
+    address: "London No. 2 Lake Park",
   },
 ];
-
-export const columnsCommodities = [
-  {
-    title: "Commodities",
-    render: (text, record) => (
-      <Link to={"/farmer/home/commodities/" + record.commodities}>{text}</Link>
+const App = () => {
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
     ),
-    dataIndex: "commodities",
-    key: "commodities",
-  },
-  {
-    title: "Percentage",
-    dataIndex: "percentage",
-    key: "percentage",
-  },
-];
-// END COMMODITY DATA
-
-// REGIONDATA
-export const dataRegion = [
-  {
-    key: "1",
-    region: "Region I (Ilocos Region)",
-    budget: "100,000",
-  },
-  {
-    key: "2",
-    region: "Region II (Cagayan Valley)",
-    budget: "100,000",
-  },
-  {
-    key: "3",
-    region: "Region III (Central Luzon)",
-    budget: "100,000",
-  },
-  {
-    key: "4",
-    region: "Region IV-A (CALABARZON)",
-    budget: "100,000",
-  },
-  {
-    key: "5",
-    region: "Region IV-B (MIMAROPA)",
-    budget: "100,000",
-  },
-  {
-    key: "6",
-    region: "Region V (Bicol Region)",
-    budget: "100,000",
-  },
-  {
-    key: "7",
-    region: "National Capital Region (NCR)",
-    budget: "100,000",
-  },
-  {
-    key: "8",
-    region: "Cordillera Administrative Region (CAR)",
-    budget: "100,000",
-  },
-];
-
-export const columnsRegion = [
-  {
-    title: "Region",
-    render: (text, record) => (
-      <Link to={"/farmer/home/commodities/Banana/" + record.region}>
-        {text}
-      </Link>
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1890ff" : undefined,
+        }}
+      />
     ),
-    dataIndex: "region",
-    key: "region",
-  },
-  {
-    title: "Budget",
-    dataIndex: "budget",
-    key: "budget",
-  },
-];
-
-// END REGION DATA
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: "#ffc069",
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      width: "30%",
+      ...getColumnSearchProps("name"),
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      width: "20%",
+      ...getColumnSearchProps("age"),
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      ...getColumnSearchProps("address"),
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortDirections: ["descend", "ascend"],
+    },
+  ];
+};
--- src/pages/farmer/Home/demand/Demands.jsx
+++ src/pages/farmer/Home/demand/Demands.jsx
@@ -1,17 +1,115 @@
-import { Table } from "antd";
-import {
-  columnsCommodities,
-  dataCommodities,
-} from "@/components/Demands/post-data";
+import { Alert, Avatar, Table, Tooltip } from "antd";
+import { columns, data } from "@/components/Demands/post-data";
+import Page from "@/components/Page";
+import UserOutlined from "@/icons/heroicons/UserOutlined";
+import { AntDesignOutlined } from "@ant-design/icons";
 
 export const Demands = () => {
   return (
-    <div className=" bg-white p-2">
-      <Table
+    <Page className="bg-white p-2">
+      <Alert
+        message="Find Your Market Opportunities—top products currently in demand, the percentage  shows the proportion of each vendor's total demand that is attributed to each commodity. Click it to see more details."
+        type="info"
+        showIcon
+        closable
+      />
+      <div className="border border-x-0 border-b border-t-0 border-slate-100 p-2">
+        <div className="flex flex-row items-center">
+          <div className="grow text-lg font-bold text-black">Mango</div>
+          <Avatar.Group
+            className="grow justify-center"
+            maxCount={3}
+            maxPopoverTrigger="click"
+            size="large"
+            maxStyle={{
+              color: "#f56a00",
+              backgroundColor: "#fde3cf",
+              cursor: "pointer",
+            }}
+          >
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            <Avatar style={{ backgroundColor: "#f56a00" }}>K</Avatar>
+            <Tooltip title="Ant User" placement="top">
+              <Avatar
+                style={{ backgroundColor: "#87d068" }}
+                icon={<UserOutlined />}
+              />
+            </Tooltip>
+            <Avatar
+              style={{ backgroundColor: "#1890ff" }}
+              icon={<AntDesignOutlined />}
+            />
+          </Avatar.Group>
+          <div className="grow text-center text-slate-500">70%</div>
+        </div>
+      </div>
+      <div className="border border-x-0 border-b border-t-0 border-slate-100 p-2">
+        <div className="flex flex-row items-center">
+          <div className="grow text-lg font-bold text-black">Eggplant</div>
+          <Avatar.Group
+            className="grow justify-center"
+            maxCount={3}
+            maxPopoverTrigger="click"
+            size="large"
+            maxStyle={{
+              color: "#f56a00",
+              backgroundColor: "#fde3cf",
+              cursor: "pointer",
+            }}
+          >
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            <Avatar style={{ backgroundColor: "#f56a00" }}>K</Avatar>
+            <Tooltip title="Ant User" placement="top">
+              <Avatar
+                style={{ backgroundColor: "#87d068" }}
+                icon={<UserOutlined />}
+              />
+            </Tooltip>
+            <Avatar
+              style={{ backgroundColor: "#1890ff" }}
+              icon={<AntDesignOutlined />}
+            />
+          </Avatar.Group>
+          <div className="grow text-center text-slate-500">80%</div>
+        </div>
+      </div>
+      <div className="border border-x-0 border-b border-t-0 border-slate-100 p-2">
+        <div className="flex flex-row items-center">
+          <div className="grow text-lg font-bold text-black">Pinapple</div>
+          <Avatar.Group
+            className="grow justify-center"
+            maxCount={3}
+            maxPopoverTrigger="click"
+            size="large"
+            maxStyle={{
+              color: "#f56a00",
+              backgroundColor: "#fde3cf",
+              cursor: "pointer",
+            }}
+          >
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            <Avatar style={{ backgroundColor: "#f56a00" }}>J</Avatar>
+            <Tooltip title="Ant User" placement="top">
+              <Avatar
+                style={{ backgroundColor: "#87d068" }}
+                icon={<UserOutlined />}
+              />
+            </Tooltip>
+            <Avatar
+              style={{ backgroundColor: "#1890ff" }}
+              icon={<AntDesignOutlined />}
+            />
+          </Avatar.Group>
+          <div className="grow text-center text-slate-500">90%</div>
+        </div>
+      </div>
+      {/* <Table
+        className="justify-center text-center"
         dataSource={dataCommodities}
         columns={columnsCommodities}
         pagination={false}
-      />
-    </div>
+      /> */}
+      <Table columns={columns} dataSource={data} />;
+    </Page>
   );
 };
