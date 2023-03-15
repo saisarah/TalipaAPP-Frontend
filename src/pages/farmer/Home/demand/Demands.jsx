099e00e1e65abf5cc6a94323153151341bedf119
Uno-count <paonemorfi@gmail.com>
2023-03-15 16:08:51 +0800

    Update Demands component to add vendor avatar group and view demands button

--- src/pages/farmer/Home/demand/Demands.jsx
+++ src/pages/farmer/Home/demand/Demands.jsx
@@ -1,7 +1,11 @@
 import { Alert, Avatar, Button, Input, Space, Table, Tag, Tooltip } from "antd";
 import Page from "@/components/Page";
 import UserOutlined from "@/icons/heroicons/UserOutlined";
-import { AntDesignOutlined, RightOutlined } from "@ant-design/icons";
+import {
+  AntDesignOutlined,
+  ArrowUpOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
 import { dataCommodities } from "./post-data";
 import { Link } from "react-router-dom";
 
@@ -15,7 +19,7 @@ export const Demands = () => {
         closable
       />
 
-      <div className="p-4">
+      {/* <div className="p-4">
         <table className="w-full table-auto">
           <thead className="bottom-1 mx-2 border border-x-0 border-t-0 ">
             <tr className="">
@@ -77,7 +81,66 @@ export const Demands = () => {
             ))}
           </tbody>
         </table>
-      </div>
+      </div> */}
+
+      {dataCommodities.map((item) => (
+        <Link key={item.key}>
+          <div className="mt-2 rounded border border-[#e5e7eb] p-4 py-2 text-base shadow-sm">
+            <div className="flex justify-between">
+              <div className="text-xl font-bold leading-tight text-black ">
+                {item.commodities} <br />
+                <span className="text-sm font-thin text-slate-400">
+                  Requested commodity
+                </span>
+              </div>
+              <div className="px-6 pt-1 text-4xl font-bold text-black">
+                {item.percentage}%
+              </div>
+            </div>
+
+            <div className="flex justify-between  border-b-2 py-2 ">
+              <div className="">
+                <Avatar.Group
+                  className="align-middle"
+                  maxCount={4}
+                  maxPopoverTrigger="click"
+                  size="large"
+                  maxStyle={{
+                    color: "#f56a00",
+                    backgroundColor: "#fde3cf",
+                    cursor: "pointer",
+                  }}
+                >
+                  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+
+                  <Avatar style={{ backgroundColor: "#f56a00" }}>J</Avatar>
+                  <Avatar src="" />
+                  <Tooltip title="Ant User" placement="top">
+                    <Avatar
+                      style={{ backgroundColor: "#87d068" }}
+                      icon={<UserOutlined />}
+                    />
+                  </Tooltip>
+
+                  <Avatar
+                    style={{ backgroundColor: "#1890ff" }}
+                    icon={<AntDesignOutlined />}
+                  />
+                </Avatar.Group>
+              </div>
+              <div className="pt-2 text-sm font-thin text-black">
+                Requested by N vendors
+              </div>
+            </div>
+            <div className="w-full pt-2 text-right">
+              <button className="rounded border px-2 font-normal text-black">
+                View Demands
+                <ArrowUpOutlined className="ml-2 rotate-45" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      ))}
     </Page>
   );
 };
