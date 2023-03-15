ce9b323acc2683dd0e1421869a805f34707ad0bd
Uno-count <paonemorfi@gmail.com>
2023-03-16 06:34:52 +0800

    Add logic to handle account type selection on ChooseAccountType form

--- src/pages/auth/register/forms/ChooseAccountType.jsx
+++ src/pages/auth/register/forms/ChooseAccountType.jsx
@@ -1,10 +1,36 @@
 import { LeftOutlined } from "@ant-design/icons";
 import { Button } from "antd";
+import React from "react";
 import { Link } from "react-router-dom";
 import farmerImg from "../images/farmer.png";
 import vendorImg from "../images/vendor.png";
 
 export default function ChooseAccountType({ setAccountType }) {
+  const [buttonClicked, setButtonClicked] = React.useState(false);
+
+  function handleButtonClick() {
+    setButtonClicked(true);
+
+    const farmerBtn = document.getElementById("farmer-button");
+    if (farmerBtn) {
+      farmerBtn.style.display = "none";
+    }
+    setTimeout(() => {
+      setAccountType("vendor");
+    }, 2000);
+  }
+
+  function handleFarmerButtonClick() {
+    setButtonClicked(true);
+    const vendorBtn = document.getElementById("vendor-button");
+    if (vendorBtn) {
+      vendorBtn.style.display = "none";
+    }
+    setTimeout(() => {
+      setAccountType("farmer");
+    }, 2000);
+  }
+
   return (
     <div className="flex h-screen flex-col">
       <Link to="/login">
@@ -15,8 +41,11 @@ export default function ChooseAccountType({ setAccountType }) {
         />
       </Link>
       <button
-        onClick={() => setAccountType("vendor")}
-        className="relative grid flex-shrink-0 flex-grow place-content-center"
+        id="vendor-button"
+        onClick={handleButtonClick}
+        className={`relative grid flex-shrink-0 ${
+          buttonClicked ? "h-full" : "h-[50%]"
+        } place-content-center transition-all duration-500 ease-in-out`}
       >
         <img
           className="absolute inset-0 h-full w-full  object-cover  "
@@ -29,8 +58,11 @@ export default function ChooseAccountType({ setAccountType }) {
         </span>
       </button>
       <button
-        onClick={() => setAccountType("farmer")}
-        className="relative grid flex-shrink-0 flex-grow place-content-center"
+        id="farmer-button"
+        onClick={handleFarmerButtonClick}
+        className={`relative grid flex-shrink-0 ${
+          buttonClicked ? "h-screen" : "h-[50%] "
+        } place-content-center transition-all duration-500 ease-in-out`}
       >
         <img
           className="absolute inset-0 h-full w-full object-cover"
