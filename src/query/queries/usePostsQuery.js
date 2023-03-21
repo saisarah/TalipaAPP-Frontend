bc38c650f028e4c77a154b555dbf2a65f9ee53ee
lenard123 <lenard.mangayayam@gmail.com>
2023-03-21 19:25:19 +0000

    Refactor ForSale.jsx to use the new usePostsQuery hook instead of fetchPosts

--- src/query/queries/usePostsQuery.js
+++ src/query/queries/usePostsQuery.js
@@ -1,6 +1,10 @@
-import { fetchPost } from "@/apis/PostApi";
+import { fetchPost, fetchPosts } from "@/apis/PostApi";
 import { useQuery } from "@tanstack/react-query";
 
 export const usePostQuery = (post_id) => {
   return useQuery(["posts", post_id], () => fetchPost(post_id));
 };
+
+export const usePostsQuery = () => {
+  return useQuery(["posts"], fetchPosts);
+}
