80d378d808623de3c8fd550fba88b22a3ebe23a1
lenard123 <lenard.mangayayam@gmail.com>
2023-03-21 19:22:20 +0000

    Add a new query hook for fetching a single farmer group by ID and refactor GroupInfo component to use it

--- src/apis/FarmerGroupApi.js
+++ src/apis/FarmerGroupApi.js
@@ -4,3 +4,9 @@ export const fetchFarmerGroups = async function () {
   const result = await Http.get("farmer-groups");
   return result.data;
 };
+
+
+export const fetchGroup = async (id) => {
+  const { data } = await Http.get(`/farmer-groups/${id}`);
+  return data;
+};
