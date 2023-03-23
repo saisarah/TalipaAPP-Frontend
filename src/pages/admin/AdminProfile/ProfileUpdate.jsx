c3109a11a1d92c08e45d485b5756ceb08a758db9
Josh <villanuevajoshua27@gmail.com>
2023-03-23 17:36:09 +0800

    update avatar in profile update

--- src/pages/admin/AdminProfile/ProfileUpdate.jsx
+++ src/pages/admin/AdminProfile/ProfileUpdate.jsx
@@ -1,13 +1,127 @@
-import { Button, Card, DatePicker, Dropdown, Form, Input, Select } from "antd";
-import { SaveOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Card,
+  DatePicker,
+  Dropdown,
+  Form,
+  Input,
+  Select,
+  Upload,
+  message,
+} from "antd";
+import { SaveOutlined, PlusOutlined, LoadingOutlined } from "@ant-design/icons";
 import FormItem from "@/components/FormItem";
 import TextArea from "antd/lib/input/TextArea";
+
+import { useState } from "react";
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+};
+const beforeUpload = (file) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
+
 export default function ProfileUpdate() {
   const onChange = (date, dateString) => {
     console.log(date, dateString);
   };
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+  const handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
   return (
     <>
+      <Card
+        className="mb-3 p-4"
+        title={<p className="font-bold uppercase">Update Avatar</p>}
+      >
+        {/* <Upload
+          name="avatar"
+          listType="picture-card"
+          className="avatar-uploader"
+          showUploadList={false}
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          beforeUpload={beforeUpload}
+          onChange={handleChange}
+        >
+          {imageUrl ? (
+            <img
+              src={imageUrl}
+              alt="avatar"
+              style={{
+                width: "100%",
+              }}
+            />
+          ) : (
+            uploadButton
+          )}
+        </Upload> */}
+        <Form>
+          <div className="flex justify-center rounded-full">
+            <Upload
+              name="avatar"
+              listType="picture-circle"
+              className="avatar-uploader rounded-full"
+              showUploadList={false}
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              beforeUpload={beforeUpload}
+              onChange={handleChange}
+            >
+              {imageUrl ? (
+                <img
+                  src={imageUrl}
+                  alt="avatar"
+                  style={{
+                    width: "100%",
+                  }}
+                />
+              ) : (
+                uploadButton
+              )}
+            </Upload>
+          </div>
+          <div className="mb-4 flex justify-end">
+            <Button type="primary" className="rounded" htmlType="submit">
+              <SaveOutlined />
+              Save Changes
+            </Button>
+          </div>
+        </Form>
+      </Card>
+
       <Card
         className="mb-3 p-4"
         title={
