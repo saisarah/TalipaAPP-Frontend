a17e6c2a8a92a3618b0a58ba144a90695e6d815e
Josh <villanuevajoshua27@gmail.com>
2023-03-23 15:19:10 +0800

    admin profile margin update

--- src/pages/admin/AdminProfile/Profile.jsx
+++ src/pages/admin/AdminProfile/Profile.jsx
@@ -1,12 +1,11 @@
 import { EditOutlined } from "@ant-design/icons";
 import { Button, Card } from "antd";
 import { Link } from "react-router-dom";
-
 export default function Profile() {
   return (
     <>
       <Card
-        className="p-4"
+        className="mb-3 p-4"
         title={<p className="font-bold uppercase">Account Information</p>}
         extra={
           <Button type="primary">
@@ -17,40 +16,52 @@ export default function Profile() {
           </Button>
         }
       >
-        <div className="grid grid-rows-2">
-          <div className="my-3 grid grid-cols-3 gap-4 border-b-2">
-            <div>
-              <img
-                src="https://via.placeholder.com/200"
-                className="mb-3 rounded-full"
-              />
-            </div>
-            <div className="col-span-2">
-              <p className="text-lg font-bold uppercase">joshua villanueva</p>
-              <p>
-                <span className="text-primary">
-                  villanuevajoshua27@gmail.com
-                </span>
-                <span> - Administrator</span>
-              </p>
-            </div>
+        <div className="mb-3 grid grid-cols-3 gap-4 border-b-2">
+          <div>
+            <img
+              src="https://via.placeholder.com/200"
+              className="mb-3 rounded-full"
+            />
           </div>
+          <div className="col-span-2">
+            <p className="text-lg font-bold uppercase">joshua villanueva</p>
+            <p>
+              <span className="text-primary">villanuevajoshua27@gmail.com</span>
+              <span> - Administrator</span>
+            </p>
+          </div>
+        </div>
 
-          <div className="my-3 grid grid-cols-3 gap-4 pb-3">
-            <div>
-              <p className="font-bold">Gender</p>
-              <p className="font-bold">Birthday</p>
-              <p className="font-bold">Contact number</p>
-              <p className="font-bold">Address</p>
-              <p className="font-bold">Account created</p>
-            </div>
-            <div className="col-span-2">
-              <p>Male</p>
-              <p>January 23, 2000</p>
-              <p>+63 976 140 1847</p>
-              <p>A. Bonifacio Ave. Brgy. Sto. Domingo Cainta Rizal</p>
-              <p>July 23, 2019</p>
-            </div>
+        <div className="mb-3 grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <p className="font-bold uppercase">Gender</p>
+          </div>
+          <div className="col-span-2">
+            <p>Male</p>
+          </div>
+        </div>
+        <div className="mb-3 grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <p className="font-bold uppercase">Birthday</p>
+          </div>
+          <div className="col-span-2">
+            <p>January 23, 2019</p>
+          </div>
+        </div>
+        <div className="mb-3 grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <p className="font-bold uppercase">Contact number</p>
+          </div>
+          <div className="col-span-2">
+            <p>+639 140 1847</p>
+          </div>
+        </div>
+        <div className="mb-3 grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <p className="font-bold uppercase">Address</p>
+          </div>
+          <div className="col-span-2">
+            <p>A. Bonifacio Ave. Brgy. Sto. Domingo Cainta Rizal</p>
           </div>
         </div>
       </Card>
