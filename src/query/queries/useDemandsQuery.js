33c8558857a4bc2c7deec5669db0df1c3607443f
lenard123 <lenard.mangayayam@gmail.com>
2023-03-23 05:54:55 +0000

    Add demand fetching and rendering logic to Demands component and implement form submission in AddDemand component

--- src/query/queries/useDemandsQuery.js
+++ src/query/queries/useDemandsQuery.js
@@ -2,6 +2,6 @@ import { fetchDemands } from "@/apis/DemandApi"
 import { useQuery } from "@tanstack/react-query"
 
 
-export const useDemandsQuery = ({ crop_id }) => {
+export const useDemandsQuery = ({ crop_id } = {}) => {
     return useQuery(['demands', { crop_id }], () => fetchDemands({crop_id}))
 }
