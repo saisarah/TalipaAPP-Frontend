52a881fbd58d1da11aaf2b9483e6349586e97b5c
Josh <villanuevajoshua27@gmail.com>
2023-03-23 16:03:07 +0800

    vendor profile

--- src/pages/admin/UserManagement/Vendor/Profile.jsx
+++ src/pages/admin/UserManagement/Vendor/Profile.jsx
@@ -1,48 +1,55 @@
-import { Card, Col, Row } from "antd";
+import { Card } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   return (
     <>
-      <div className="rounded bg-white p-4">
-        <h1>Vendors Profile</h1>
-
-        <div className="my-4 flex flex-grow justify-around gap-4">
+      <Card
+        className="mb-3 p-4"
+        title={<p className="font-bold uppercase">Vendor Information</p>}
+      >
+        <div className="mb-3 grid grid-cols-3 gap-4 border-b-2">
           <div>
             <img
               src="https://via.placeholder.com/200"
-              className="rounded-full"
+              className="mb-3 rounded-full"
             />
           </div>
-
-          <div className="my-2 flex-col">
-            <h1>Personal Details</h1>
-            <hr />
-            <p>Name</p>
-            <h1>Joshua VIllanueva</h1>
-            <p>Gender</p>
-            <h1>Male</h1>
-            <p>Phone</p>
-            <h1>+63 976 007 0541</h1>
-            <p>Email</p>
-            <h1>lorem@gmail.com</h1>
-            <p>Address</p>
-            <h1>
+          <div className="col-span-2">
+            <p className="text-lg font-bold uppercase">
+              Joshua Alfaro Villanueva
+            </p>
+            <p>
+              <Link class="text-primary" to="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=villanuevajoshua27@gmail.com">
+                villanuevajoshua27@gmail.com
+              </Link>
+              <Link to="tel:+63 976 007 0541"> | +63 976 007 0541</Link>
+            </p>
+            <p>Male</p>
+            <p>
               Zamboanga del Sur, Zamboanga City, G/F Interco Building, N. S.
               Valderrosa Street
-            </h1>
-          </div>
-          <div className="my-2 flex-col">
-            <h1>Vendors Details</h1>
-            <hr />
-            <p>Vendor Area</p>
-            <h1>5 - hectares</h1>
-            <p>Vendor Type</p>
-            <h1>Irrigated</h1>
-            <p>Ownership type</p>
-            <h1>Registered Owner</h1>
+            </p>
           </div>
         </div>
-      </div>
+
+        <div className="mb-3 grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <p className="font-bold uppercase">Market place</p>
+          </div>
+          <div className="col-span-2">
+            <p>225 N. Garcia Street, Makati City</p>
+          </div>
+        </div>
+        <div className="mb-3 grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <p className="font-bold uppercase">Commodities</p>
+          </div>
+          <div className="col-span-2">
+            <p>Banana, Eggplant</p>
+          </div>
+        </div>
+      </Card>
     </>
   );
 }
