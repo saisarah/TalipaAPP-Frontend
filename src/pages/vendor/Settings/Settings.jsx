aab6e55329c5fc8cc3c33a89154ad89d7bbda788
saisarah <sarahoben7@gmail.com>
2023-03-24 12:07:24 +0800

    Update user details to display correct information

--- src/pages/vendor/Settings/Settings.jsx
+++ src/pages/vendor/Settings/Settings.jsx
@@ -8,8 +8,8 @@ import { Link } from "react-router-dom";
 import ChangeNames from "./Forms/form/ChangeName";
 
 export default function Settings() {
+  
   const { data: user } = useCurrentUserQuery();
-
   useTitle("Settings");
   return (
     <Page className="bg-white text-black">
@@ -33,10 +33,8 @@ export default function Settings() {
 
         <div className="flex w-full justify-center p-4 text-center">
           <div className=" leading-4">
-            <div className="text-xl font-bold">
-              {user.firstname} {user.lastname}
-            </div>
-            <span className="text-slate-500">Sarah Oben's Store</span>
+            <div className="text-xl font-bold">{user.fullname}</div>
+            <span className="text-slate-500">{user.user_type}</span>
           </div>
         </div>
       </div>
@@ -50,7 +48,7 @@ export default function Settings() {
         >
           <div>
             Fullname
-            <div className="text-slate-400">Sarah Oben</div>
+            <div className="text-slate-400">{user.fullname}</div>
           </div>
           <div>
             <RightOutlined />
@@ -63,7 +61,7 @@ export default function Settings() {
         >
           <div>
             Contact Number
-            <div className="text-slate-400">0999 099 0999</div>
+            <div className="text-slate-400">{user.contact_number}</div>
           </div>
           <div>
             <RightOutlined />
@@ -76,7 +74,7 @@ export default function Settings() {
         >
           <div>
             Email address
-            <div className="text-slate-400">saisarah@gmail.com</div>
+            <div className="text-slate-400">{user.email}</div>
           </div>
           <div>
             <RightOutlined />
@@ -89,7 +87,7 @@ export default function Settings() {
         >
           <div>
             Gender
-            <div className="text-slate-400">Female</div>
+            <div className="text-slate-400">{user.gender}</div>
           </div>
           <div>
             <RightOutlined />
@@ -102,7 +100,7 @@ export default function Settings() {
         >
           <div>
             Username
-            <div className="text-slate-400">Sarah#2463</div>
+            <div className="text-slate-400">{user.username}</div>
           </div>
           <div>
             <RightOutlined />
@@ -129,9 +127,7 @@ export default function Settings() {
         >
           <div>
             Delivery Address
-            <div className="text-slate-400">
-              Phase 1 Bagong Silang Caloocan City
-            </div>
+            <div className="text-slate-400">Ph 1 Bagong Silang</div>
           </div>
           <div>
             <RightOutlined />
