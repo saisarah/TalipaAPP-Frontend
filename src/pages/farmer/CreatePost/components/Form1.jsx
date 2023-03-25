99188af77c7e83f503c172bbdcc4bb7929a094c3
lenard123 <lenard.mangayayam@gmail.com>
2023-03-25 12:04:52 +0000

    Refactor crop options in Form1.jsx to use the full crop object instead of just name and id for better data consistency

--- src/pages/farmer/CreatePost/components/Form1.jsx
+++ src/pages/farmer/CreatePost/components/Form1.jsx
@@ -31,7 +31,7 @@ export default function Form1({ step, setStep, formData }) {
       >
         <Select
           placeholder="Select Commodity"
-          options={crops.map(({ name, id }) => ({ value: id, label: name }))}
+          options={crops}
           size="large"
           loading={fetchingCrops}
         />
