f2786b2886928e09b6572717640d13ae8c6600ee
lenard123 <lenard.mangayayam@gmail.com>
2023-03-27 19:32:40 +0000

    Add FarmerAvatar component and refactor Navbar in FarmerLayout.jsx

--- src/components/FarmerLayout/FarmerLayout.jsx
+++ src/components/FarmerLayout/FarmerLayout.jsx
@@ -1,33 +1,99 @@
-import logo from "@/assets/logo-text.svg"
-import { useAppContext } from "@/contexts/AppContext"
-import { useFarmerContext } from "@/contexts/FarmerContext"
-import { Avatar } from "antd"
-import { NavLink as RouterNavLink } from "react-router-dom"
+import logo from "@/assets/logo-text.svg";
+import { useAppContext } from "@/contexts/AppContext";
+import { useFarmerContext } from "@/contexts/FarmerContext";
+import { useLogoutModal } from "@/hooks/useLogoutModal";
+import {
+  LogoutOutlined,
+  QuestionCircleFilled,
+  SettingFilled,
+} from "@ant-design/icons";
+import { Avatar, Button, Popover } from "antd";
+import { useRef } from "react";
+import { Link, NavLink as RouterNavLink } from "react-router-dom";
 
 const NavLink = ({ children, ...props }) => {
-  const activeClassName = "font-bold text-primary border-b-2 border-primary"
-  const className = "text-base text-slate-500" 
+  const activeClassName = "font-bold text-primary border-b-2 border-primary";
+  const className = "text-base text-slate-500";
   return (
-    <RouterNavLink 
-      className={({isActive}) => `${isActive ? activeClassName:''} ${className}`}
+    <RouterNavLink
+      className={({ isActive }) =>
+        `${isActive ? activeClassName : ""} ${className}`
+      }
       {...props}
-      >
+    >
       {children}
     </RouterNavLink>
-  )
-}
+  );
+};
 
-const Navbar = () => {
+const FarmerAvatar = () => {
+  const { user } = useFarmerContext();
+  const { confirmLogout } = useLogoutModal();
+  const avatar = useRef()
 
-  const { user } = useFarmerContext()
+  const handleClick = ({ target }) => {
+    avatar.current.click()
+  }
 
   return (
-    <header className="z-10 bg-white h-16 shadow-md fixed inset-x-0 top-0 px-4 flex items-center justify-center">
-      <nav className="container flex items-center gap-12 max-w-screen-lg ">
-        <div>
-          <img src={logo} className="h-12"/>
+    <Popover
+      placement="bottomRight"
+      content={
+        <div className="min-w-[200px]" onClick={handleClick}>
+          <div className="flex items-center gap-2 border-b pb-2">
+            <Avatar src={user.profile_picture} />
+            <div className="font-bold">{user.fullname}</div>
+          </div>
+          <Link to="/farmer/profile">See profile</Link>
+
+          <div className="mt-4">
+            <Link
+              to="/farmer/settings"
+              className="-mx-4 flex items-center gap-2 py-1 px-4 font-bold text-inherit hover:bg-slate-200 hover:text-inherit"
+            >
+              <span className="rounded-full bg-slate-200 p-2 leading-[0]">
+                <SettingFilled />
+              </span>
+              <span>Settings</span>
+            </Link>
+            <Link
+              to="/farmer/help"
+              className="-mx-4 flex items-center gap-2 py-1 px-4 font-bold text-inherit hover:bg-slate-200 hover:text-inherit"
+            >
+              <span className="rounded-full bg-slate-200 p-2 leading-[0]">
+                <QuestionCircleFilled />
+              </span>
+              <span>Help</span>
+            </Link>
+            <Link
+              onClick={() => {
+                confirmLogout();
+              }}
+              className="-mx-4 flex items-center gap-2 py-1 px-4 font-bold text-inherit hover:bg-slate-200 hover:text-inherit"
+            >
+              <span className="rounded-full bg-slate-200 p-2 leading-[0]">
+                <LogoutOutlined />
+              </span>
+              <span>Logout</span>
+            </Link>
+          </div>
         </div>
-        <div className="flex gap-6 flex-grow">
+      }
+      trigger="click"
+    >
+      <Avatar ref={avatar} src={user.profile_picture} />
+    </Popover>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <header className="fixed inset-x-0 top-0 z-10 flex h-16 items-center justify-center bg-white px-4 shadow-md">
+      <nav className="container flex max-w-screen-lg items-center gap-12 ">
+        <div>
+          <img src={logo} className="h-12" />
+        </div>
+        <div className="flex flex-grow gap-6">
           <NavLink to="/farmer/home">Home</NavLink>
           <NavLink to="/farmer/messages">Messages</NavLink>
           <NavLink to="/farmer/notifications">Notifications</NavLink>
@@ -36,26 +102,25 @@ const Navbar = () => {
           <NavLink to="/farmer/wallet">Wallet</NavLink>
         </div>
         <div>
-          <Avatar src={user.profile_picture} />
+          <FarmerAvatar />
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default function FarmerLayout({ children }){
-
-  const { viewport } = useAppContext()
+export default function FarmerLayout({ children }) {
+  const { viewport } = useAppContext();
 
   if (viewport.isMedium)
     return (
-      <div className="pt-16 min-h-screen flex flex-col">
+      <div className="flex min-h-screen flex-col pt-16">
         <Navbar />
-        <div className="max-w-screen-lg flex-grow flex flex-col mx-auto w-full">
+        <div className="mx-auto flex w-full max-w-screen-lg flex-grow flex-col">
           {children}
         </div>
       </div>
-    )
+    );
 
-  return children
+  return children;
 }
