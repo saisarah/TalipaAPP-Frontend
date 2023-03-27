c30312dc7faa9ed984be26b3a160410a3b1012e4
lenard123 <lenard.mangayayam@gmail.com>
2023-03-27 11:02:39 +0000

    Make CropDemand Page responsive

--- src/pages/farmer/Demands/CropsDemands.jsx
+++ src/pages/farmer/Demands/CropsDemands.jsx
@@ -19,7 +19,7 @@ export const CropsDemands = () => {
   );
 
   return (
-    <div className="bg-white p-2">
+    <div className="bg-white p-4 md:bg-transparent md:py-0">
       <Alert
         message="Find Your Market Opportunities—top products currently in demand, the percentage  shows the proportion of each vendor's total demand that is attributed to each commodity. Click it to see more details."
         type="info"
@@ -27,20 +27,22 @@ export const CropsDemands = () => {
         closable
       />
 
-      {data.map((crop) =>
-        crop.demands.length ? (
-          <CropDemandsCard
-            key={crop.id}
-            id={crop.id}
-            name={crop.name}
-            percentage={total && (crop.demands_sum_budget * 100) / total}
-            request_count={crop.demands.length}
-            avatars={crop.demands.map(
-              (demand) => demand.author.profile_picture
-            )}
-          />
-        ) : null
-      )}
+      <div className="grid gap-2 py-4 md:grid-cols-2">
+        {data.map((crop) =>
+          crop.demands.length ? (
+            <CropDemandsCard
+              key={crop.id}
+              id={crop.id}
+              name={crop.name}
+              percentage={total && (crop.demands_sum_budget * 100) / total}
+              request_count={crop.demands.length}
+              avatars={crop.demands.map(
+                (demand) => demand.author.profile_picture
+              )}
+            />
+          ) : null
+        )}
+      </div>
     </div>
   );
 };
