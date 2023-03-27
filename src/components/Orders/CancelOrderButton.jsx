e233f4f6e64e7f0d00058644135976db9d3f6363
saisarah <sarahoben7@gmail.com>
2023-03-27 20:36:50 +0800

    Add Cancel Order mutation and component to Orders flow

--- src/components/Orders/CancelOrderButton.jsx
+++ src/components/Orders/CancelOrderButton.jsx
@@ -1,17 +1,31 @@
+import { useCancelOrder } from "@/query/mutations/useCancelOrder";
 import { Button, Modal } from "antd";
+import { useNavigate } from "react-router-dom";
 
 export const CancelOrderButton = ({ id }) => {
-    const handleClick = () => {
-      Modal.confirm({
-        content: "Are you sure to cancel this order?",
-        onOk() {},
-      });
-    };
-  
-    return (
-      <Button onClick={handleClick} danger size="large"  block>
-        Cancel Order
-      </Button>
-    );
+  const navigate = useNavigate();
+  const { mutate, isLoading } = useCancelOrder(id, {
+    onSuccess() {
+      navigate("/farmer/orders?tab=cancelled");
+    },
+
+    onError(err) {
+      notification.error({ message: getErrorMessage(err) });
+    },
+  });
+
+  const handleClick = () => {
+    Modal.confirm({
+      content: "Are you sure to cancel this order?",
+      onOk() {
+        mutate();
+      },
+    });
   };
-  
+
+  return (
+    <Button loading={isLoading} onClick={handleClick} danger size="large" block>
+      Cancel Order
+    </Button>
+  );
+};
