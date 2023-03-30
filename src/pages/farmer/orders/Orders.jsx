4257a60c5505940f4adacf727b7cb6d74380ccaf
lenard123 <lenard.mangayayam@gmail.com>
2023-03-30 04:47:39 +0000

    Refactor OrderTabLinks component to accept a className prop and add it to the TabLinks component for more flexibility in styling. Update Orders page to include a fixed header with the OrderTabLinks component and adjust page layout

--- src/pages/farmer/orders/Orders.jsx
+++ src/pages/farmer/orders/Orders.jsx
@@ -1,21 +1,23 @@
 import Page from "@/components/Page";
-import PageHeader from "@/components/PageHeader/PageHeader";
 import { useTab } from "@/helpers/hooks";
-import OrderLists from '@/components/Orders/OrderLists';
+import OrderLists from "@/components/Orders/OrderLists";
 import OrderTabLinks from "@/components/Orders/OrderTabLinks";
+import FarmerPageHeader from "@/components/PageHeader/FarmerPageHeader";
 export default function Orders() {
   const tabs = ["pending", "processing", "shipped", "cancelled", "completed"];
   const { selected } = useTab(tabs);
 
   return (
-    <Page className="bg-slate-50">
-      <PageHeader back="/farmer" title="Orders" />
+    <Page className="md:pt-16">
+      <FarmerPageHeader back="/farmer" title="Orders" />
 
-      <OrderTabLinks tabs={tabs} />
-
+      <div className="md:fixed md:inset-x-0 md:top-16 md:z-10 md:bg-white md:shadow-md">
+        <OrderTabLinks tabs={tabs} className="max-w-screen-lg mx-auto md:shadow-none"/>
+      </div>
+      
       <OrderLists
         className="flex flex-col gap-2 p-2"
-        cardLink={id => `/farmer/orders/${id}`}
+        cardLink={(id) => `/farmer/orders/${id}`}
         status={selected}
       />
     </Page>
