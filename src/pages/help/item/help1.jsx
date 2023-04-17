24c90a1636334292d5cbf09ba672e603a6d69db3
r-kyle <kylerubante@gmail.com>
2023-04-17 17:11:22 +0800

    help items

--- src/pages/help/item/help1.jsx
+++ src/pages/help/item/help1.jsx
@@ -1,74 +1,76 @@
 import Page from "@/components/Page";
 import PageHeader from "@/components/PageHeader";
 
-<Page>
-  <PageHeader title="Welcome To TalipaAPP: A new guide for buyers.">
-    <div>
-      <h1>Welcome To TalipaAPP: A new guide for buyers.</h1>
-    </div>
+export default function help1() {
+  <Page>
+    <PageHeader title="Welcome To TalipaAPP: A new guide for buyers.">
+      <div>
+        <h1>Welcome To TalipaAPP: A new guide for buyers.</h1>
+      </div>
 
-    <div className="">
-      <h3 className="font-bold">Welcome to TalipaAPP!</h3>
-      <p>This is the start of your shopping journey in Lazada.</p>
-      <p>
-        We want to give you the best possible online shopping experience so
-        we've prepared everything that you need to know.
-      </p>
-    </div>
+      <div className="">
+        <h3 className="font-bold">Welcome to TalipaAPP!</h3>
+        <p>This is the start of your shopping journey in Lazada.</p>
+        <p>
+          We want to give you the best possible online shopping experience so
+          we've prepared everything that you need to know.
+        </p>
+      </div>
 
-    <div className="">
-      <h3 className="font-bold">2 Reasons to Checkout from TalipaAPP</h3>
-      <p>
-        Aside from the guaranteed safe and enjoyable online shopping, below are
-        the more reasons to trust TalipaAPP.
-      </p>
-    </div>
+      <div className="">
+        <h3 className="font-bold">2 Reasons to Checkout from TalipaAPP</h3>
+        <p>
+          Aside from the guaranteed safe and enjoyable online shopping, below
+          are the more reasons to trust TalipaAPP.
+        </p>
+      </div>
 
-    <div className="">
-      <p>Fast Shipping</p>
-    </div>
+      <div className="">
+        <p>Fast Shipping</p>
+      </div>
 
-    <div className="">
-      <p>Money Back Guarantee</p>
-      <p>
-        Be refunded if you don't receive the item you ordered or if there is a
-        problem with the condition of the item you received.
-      </p>
-    </div>
+      <div className="">
+        <p>Money Back Guarantee</p>
+        <p>
+          Be refunded if you don't receive the item you ordered or if there is a
+          problem with the condition of the item you received.
+        </p>
+      </div>
 
-    <div className="">
-      <h3 className="font-bold">Shop and Save</h3>
-      <p>
-        Shop and save, at the same time, through TalipaAPP's valuable items
-        posted directly through the farmer.
-      </p>
-    </div>
+      <div className="">
+        <h3 className="font-bold">Shop and Save</h3>
+        <p>
+          Shop and save, at the same time, through TalipaAPP's valuable items
+          posted directly through the farmer.
+        </p>
+      </div>
 
-    <div className="">
-      <h3 className="font-bold">Your online shopping is safe with us.</h3>
-      <p>
-        Here are articles that will help you learn about safe shopping in
-        TalipaAPP.
-      </p>
-    </div>
+      <div className="">
+        <h3 className="font-bold">Your online shopping is safe with us.</h3>
+        <p>
+          Here are articles that will help you learn about safe shopping in
+          TalipaAPP.
+        </p>
+      </div>
 
-    <div className="">
-      <h3 className="font-bold">Be constantly updated.</h3>
-      <p>
-        Know where your orders are and when you'll get them with our real-time
-        order tracking feature.
-      </p>
-    </div>
+      <div className="">
+        <h3 className="font-bold">Be constantly updated.</h3>
+        <p>
+          Know where your orders are and when you'll get them with our real-time
+          order tracking feature.
+        </p>
+      </div>
 
-    <div className="font-bold">
-      <h4>Have other concerns?</h4>
-    </div>
+      <div className="font-bold">
+        <h4>Have other concerns?</h4>
+      </div>
 
-    <div className="">
-      <p>
-        For other related concerns, click on the button below to connect with
-        our Customare Care Service.
-      </p>
-    </div>
-  </PageHeader>
-</Page>;
+      <div className="">
+        <p>
+          For other related concerns, click on the button below to connect with
+          our Customare Care Service.
+        </p>
+      </div>
+    </PageHeader>
+  </Page>;
+}
--- src/routes/guestRoutes.jsx
+++ src/routes/guestRoutes.jsx
@@ -1,10 +1,9 @@
 import ForgotPassword from "@/pages/auth/ForgotPassword/ForgotPassword";
 import Faq from "@/pages/help/Faq";
 import Help from "@/pages/help/Help";
-
 import Login from "../pages/auth/Login/Login";
 import Register from "../pages/auth/register/Register";
-
+import help1 from "@/pages/help/item/help1";
 export default [
   {
     path: "/login",
@@ -26,4 +25,12 @@ export default [
     path: "/help",
     element: <Help />,
   },
+  {
+    path: "/help1",
+    element: <help1 />,
+  },  
+  {
+    path: "/item/help2",
+    element: <help2 />,
+  },  
 ];
