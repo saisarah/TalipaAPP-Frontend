db49b945723f1220f5fb8f0ca93e693361f945df
lenard123 <lenard.mangayayam@gmail.com>
2023-04-20 10:34:54 +0800

    Integrate Cancellation of Request

--- src/pages/farmer/group/GroupInfo/GroupInfo.jsx
+++ src/pages/farmer/group/GroupInfo/GroupInfo.jsx
@@ -9,28 +9,28 @@ import { useNavigate, useParams } from "react-router-dom";
 import Forum from "../components/Forum";
 import Join from "../components/Join";
 
-const joinGroup = async(id) => {
-  const { data } = await Http.post(`/farmer-groups/${id}/join`)
-  return data
-}
+const joinGroup = async (id) => {
+  const { data } = await Http.post(`/farmer-groups/${id}/join`);
+  return data;
+};
 
 const useJoinGroup = (id, option) => {
-  return useMutation(() => joinGroup(id), option)
-}
+  return useMutation(() => joinGroup(id), option);
+};
 
 export default function GroupInfo() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { id } = useParams();
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
   const { mutate, isLoading: isJoining } = useJoinGroup(id, {
     onSuccess() {
-      queryClient.resetQueries(["farmer-group", "pending"])
-      navigate("/farmer/groups", { replace:true })
+      queryClient.resetQueries(["farmer-group", "pending"]);
+      navigate("/farmer/groups", { replace: true });
     },
     onError(err) {
-      notification.error({ message: getErrorMessage(err) })
-    }
-  })
+      notification.error({ message: getErrorMessage(err) });
+    },
+  });
 
   const { data, isLoading } = useFarmerGroupQuery(id);
 
@@ -57,16 +57,20 @@ export default function GroupInfo() {
     <div className="app-size bg-white">
       <PageHeader back="/farmer/groups" title="Group" />
 
-      <div className="h-[80px] w-[100%] max-w-md flex-shrink-0 bg-slate-300"></div>
       <img
-        className="mx-auto h-[75px] w-[75px] max-w-md flex-shrink-0 rounded-full bg-slate-300 ring"
-        style={{ marginTop: "-45px" }}
+        className="h-[80px] w-[100%] max-w-md flex-shrink-0 bg-slate-300 object-cover"
+        src={data.image_url}
       />
       <div className="my-6 text-center">
         <h1>{data.name}</h1>
         <h1>{data.type}</h1>
         <p>Members</p>
-        <Button loading={isJoining} onClick={() => mutate()} type="primary" className="mx-auto">
+        <Button
+          loading={isJoining}
+          onClick={() => mutate()}
+          type="primary"
+          className="mx-auto"
+        >
           Join
         </Button>
       </div>
