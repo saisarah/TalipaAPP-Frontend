684e71b6439c4ec032ea98a7ab07e2c381c39df4
Uno-count <paonemorfi@gmail.com>
2023-04-21 10:58:10 +0800

    add download button and remove follow link

--- src/pages/LandingPage/components/Banner.jsx
+++ src/pages/LandingPage/components/Banner.jsx
@@ -10,7 +10,29 @@ export default function Banner() {
       <div className="flex flex-col gap-4 md:flex-row">
         <div className="mt-10 w-full max-w-lg text-center font-poppins text-5xl font-bold  text-black dark:text-white md:w-1/2 md:text-left md:text-6xl ">
           Delivering Fresh Commodities Direct to Your Market.
+          <div className="mt-10 max-w-xs ">
+            <a href="" download="">
+              <button className="flex gap-2 rounded-lg bg-white px-4 py-2 text-sm font-thin text-black  dark:bg-black dark:text-white ">
+                Download now
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
+            </a>
+          </div>
         </div>
+
         <div className="">
           <div className=" mt-16 skew-y-0 duration-500   ease-in hover:skew-y-0 md:skew-y-[20deg]">
             <div className="flex gap-16">
